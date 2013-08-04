fd28b54de62eea6b562f9e7dc0cc5fbf335d5c11
Todd Hayton <thayton@neekanee.com>
2013-08-04 11:47:26 -0400

    automate download, set pks null, temp file removal

--- deploy/job_scraper/loaddata/get_locations_data.sh
+++ deploy/job_scraper/loaddata/get_locations_data.sh
@@ -4,7 +4,9 @@
 #
 # PURPOSE: Copy previous day's location data from www.neekanee.com
 #          into this directory so that it can be loaded into this
-#          machine's database.
+#          machine's database. Untars the archive, sets pks in the
+#          files to null and then copies the json files into the
+#          current directory. Then removes temporary files.
 #
 # USAGE: Run it with no arguments ./get_locations_data.sh
 #
@@ -14,9 +16,17 @@
 ######################################################################
 
 yesterday=$( date +"%m%d%y" --date='yesterday' )
+locations_dir="neekanee_${yesterday}"
 locations_file="locations-backup-neekanee_${yesterday}.tar.gz"
+
 
 scp thayton@www.neekanee.com:$locations_file .
 if [ -f $locations_file ]; then
   tar zxvf $locations_file
+  for f in `find $locations_dir -name "*.json"`; do
+    ./set_pks_null.sh $f
+  done
+  cp ${locations_dir}/*.json .
+  rm -f $locations_file
+  rm -rf $locations_dir
 fi
