5227dccf6147a141df9a930f7526e2b06e6e9234
Todd Hayton <thayton@neekanee.com>
2013-08-17 08:29:21 -0400

    elctech is now burnside digital

--- neekanee/job_scrapers/plugins/com/elctech.py
+++ neekanee/job_scrapers/plugins/com/elctech.py
@@ -9,8 +9,6 @@ COMPANY = {
     'name': 'ELC',
     'hq': 'San Francisco, CA',
 
-    'benefits': {'vacation': []},
-
     'home_page_url': 'http://www.elctech.com',
     'jobs_page_url': 'http://elctech.com/careers',
 
@@ -22,21 +20,8 @@ class ElcJobScraper(JobScraper):
         super(ElcJobScraper, self).__init__(COMPANY)
 
     def scrape_jobs(self):
-        self.br.open(self.company.jobs_page_url)
-
-        s = soupify(self.br.response().read())
-        v = s.find('div', attrs={'class': 'container'})
-        a = {'class': 'jobpost'}
-
-        self.company.job_set.all().delete()
-
-        for d in v.findAll('div', attrs=a):
-            job = Job(company=self.company)
-            job.title = d.h5.text
-            job.url = self.br.geturl()
-            job.location = self.company.location
-            job.desc = get_all_text(d)
-            job.save()
+        # XXX ELC is now Burnside Digital 
+        pass
 
 def get_scraper():
     return ElcJobScraper()
