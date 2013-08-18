9c297fcafa5514ab5ae63e22268bd97b7b9b3c13
Todd Hayton <thayton@neekanee.com>
2013-08-18 12:11:12 -0400

    westat now uses brassring

--- neekanee/job_scrapers/plugins/com/brassring/westat.py
+++ neekanee/job_scrapers/plugins/com/brassring/westat.py
@@ -1,24 +1,37 @@
-from neekanee.jobscrapers.kenexa.kenexa import KenexaJobScraper
+from neekanee.jobscrapers.brassring.brassring import BrassringJobScraper
+from neekanee.htmlparse.soupify import soupify
 
 COMPANY = {
     'name': 'Westat',
     'hq': 'Rockville, MD',
 
     'home_page_url': 'http://www.westat.com',
-    'jobs_page_url': 'https://sjobs.brassring.com/en/asp/tg/cim_home.asp?partnerid=82&siteid=5197',
+    'jobs_page_url': 'https://sjobs.brassring.com/TGWebHost/home.aspx?partnerid=82&siteid=5197',
 
     'empcnt': [1001,5000]
 }
 
-class WestatJobScraper(KenexaJobScraper):
+class WestatJobScraper(BrassringJobScraper):
     def __init__(self):
         super(WestatJobScraper, self).__init__(COMPANY)
+        self.soupify_search_form = True
 
-    def get_title_from_td(self, td):
-        return td[3].text
+    def get_url_from_formtext(self, x):
+        s = soupify(x['AutoReq'])
+        return s.a
 
-    def get_location_from_td(self, td):
-        return self.parse_location(td[4].text)
+    def get_title_from_formtext(self, x):
+        return x['JobTitle']
+
+    def get_location_from_formtext(self, x):
+        l = x['Location']
+        l = self.parse_location(l)
+
+        return l
 
 def get_scraper():
     return WestatJobScraper()
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
