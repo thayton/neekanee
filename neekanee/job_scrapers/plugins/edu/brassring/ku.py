65e4903039402138f0b4014db662a17f137dbeee
Todd Hayton <thayton@neekanee.com>
2013-09-10 11:26:36 -0400

    now uses brassring

---
+++ neekanee/job_scrapers/plugins/edu/brassring/ku.py
@@ -0,0 +1,36 @@
+import re
+
+from neekanee.jobscrapers.brassring.brassring import BrassringJobScraper
+from neekanee.htmlparse.soupify import soupify
+
+COMPANY = {
+    'name': 'Universiy of Kansas',
+    'hq': 'Lawrence, KS',
+
+    'home_page_url': 'http://www.ku.edu',
+    'jobs_page_url': 'https://sjobs.brassring.com/TGWebHost/home.aspx?partnerid=25752&siteid=5447',
+
+    'empcnt': [10001]
+}
+
+class KuJobScraper(BrassringJobScraper):
+    def __init__(self):
+        super(KuJobScraper, self).__init__(COMPANY)
+        self.soupify_search_form = True
+        self.br.addheaders = [('User-agent', 
+                               'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7')]
+
+    def get_title_from_formtext(self, x):
+        s = soupify(x['FORMTEXT8'])
+        return s.a.text
+
+    def get_url_from_formtext(self, x):
+        s = soupify(x['FORMTEXT8'])
+        return s.a
+
+def get_scraper():
+    return KuJobScraper()
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
