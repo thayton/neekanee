27e1a9f3a98d0195a805892a720680298c27c020
Todd Hayton <thayton@neekanee.com>
2013-09-17 15:08:26 -0400

    newer peopleadmin version

--- neekanee/job_scrapers/plugins/edu/peopleadmin/wellesley.py
+++ neekanee/job_scrapers/plugins/edu/peopleadmin/wellesley.py
@@ -1,18 +1,18 @@
-from neekanee.jobscrapers.peopleadmin.PeopleAdminATS import PeopleAdminJobScraper
+from neekanee.jobscrapers.peopleadmin.peopleadmin3 import PeopleAdminJobScraper
 
 COMPANY = {
     'name': 'Wellesley College',
     'hq': 'Wellesley, MA',
 
-    'benefits': {'vacation': []},
-
     'home_page_url': 'http://www.wellesley.edu',
-    'jobs_page_url': 'https://career.wellesley.edu/applicants/jsp/shared/Welcome_css.jsp',
-
-    'gctw_chronicle': True,
+    'jobs_page_url': 'https://career.wellesley.edu',
 
     'empcnt': [501,1000]
 }
 
 def get_scraper():
     return PeopleAdminJobScraper(COMPANY)
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
