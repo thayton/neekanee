d1995936ad71c0df791ff5ed5b2f38a6a7ea400d
Todd Hayton <thayton@neekanee.com>
2013-09-19 13:15:25 -0400

    orchestrate

---
+++ neekanee/job_scrapers/plugins/io/jobscore/orchestrate.py
@@ -0,0 +1,18 @@
+from neekanee.jobscrapers.jobscore.jobscore import JobScoreJobScraper
+
+COMPANY = {
+    'name': 'Orchestrate',
+    'hq': 'Portland, OR',
+
+    'home_page_url': 'http://www.orchestrate.com',
+    'jobs_page_url': 'http://www.jobscore.com/jobs/orchestrate/',
+
+    'empcnt': [11,50]
+}
+
+def get_scraper():
+    return JobScoreJobScraper(COMPANY)
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
