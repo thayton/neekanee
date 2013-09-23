5f06c60f48625201c747ba2e2ac3834a19efbca1
Todd Hayton <thayton@neekanee.com>
2013-09-23 11:36:34 -0400

    drobo

---
+++ neekanee/job_scrapers/plugins/com/link/drobo.py
@@ -0,0 +1,60 @@
+import re, urlparse
+
+from neekanee.jobscrapers.jobscraper import JobScraper
+from neekanee.htmlparse.soupify import soupify, get_all_text, get_mailto
+
+from neekanee_solr.models import *
+
+COMPANY = {
+    'name': 'Data Robotics',
+    'hq': 'Santa Clara, CA',
+
+    'home_page_url': 'http://www.drobo.com',
+    'jobs_page_url': 'http://www.drobo.com/website/site-map/',
+
+    'empcnt': [51,200]
+}
+
+class DroboJobScraper(JobScraper):
+    def __init__(self):
+        super(DroboJobScraper, self).__init__(COMPANY)
+        self.br.addheaders = [('User-agent', 
+                               'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7')]
+
+    def scrape_job_links(self, url):
+        jobs = []
+
+        self.br.open(url)
+
+        s = soupify(self.br.response().read())
+        r = re.compile(r'^/corporate/careers/[^/]+/$')
+
+        for a in s.findAll('a', href=r):
+            job = Job(company=self.company)
+            job.title = a.text
+            job.url = urlparse.urljoin(self.br.geturl(), a['href'])
+            job.location = self.company.location
+            jobs.append(job)
+        
+        return jobs
+
+    def scrape_jobs(self):
+        job_list = self.scrape_job_links(self.company.jobs_page_url)
+        self.prune_unlisted_jobs(job_list)
+        new_jobs = self.new_job_listings(job_list)
+
+        for job in new_jobs:
+            self.br.open(job.url)
+
+            s = soupify(self.br.response().read())
+            d = s.find('div', id='ContentChannel')
+
+            job.desc = get_all_text(d)
+            job.save()
+
+def get_scraper():
+    return DroboJobScraper()
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
