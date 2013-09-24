7e3740e63f5204ee920f8348994c82d06078d17b
Todd Hayton <thayton@neekanee.com>
2013-09-24 14:19:07 -0400

    unsupported vs of resumator

--- neekanee/job_scrapers/plugins/com/resumator/clearchoice.py
+++ neekanee/job_scrapers/plugins/com/resumator/clearchoice.py
@@ -1,4 +1,9 @@
-from neekanee.jobscrapers.resumator.resumator2 import ResumatorJobScraper
+import re, urlparse
+
+from neekanee.jobscrapers.jobscraper import JobScraper
+from neekanee.htmlparse.soupify import soupify, get_all_text
+
+from neekanee_solr.models import *
 
 COMPANY = {
     'name': 'ClearChoice',
@@ -10,8 +15,54 @@ COMPANY = {
     'empcnt': [501,1000]
 }
 
+class ClearChoiceJobScraper(JobScraper):
+    def __init__(self):
+        super(ClearChoiceJobScraper, self).__init__(COMPANY)
+
+    def scrape_job_links(self, url):
+        jobs = []
+
+        self.br.open(url)
+
+        s = soupify(self.br.response().read())
+        x = {'class': 'resumator-job-title resumator-jobs-text'}
+        y = {'class': 'resumator-job-location resumator-job-heading resumator-jobs-text'}
+        
+        for d in s.findAll('div', attrs=x):
+            d = d.findParent('div')
+            p = d.find('span', attrs=y)
+            l = self.parse_location(p.nextSibling)
+
+            if not l:
+                continue
+
+            job = Job(company=self.company)
+            job.title = d.a.text
+            job.url = urlparse.urljoin(self.br.geturl(), d.a['href'])
+            job.location = l
+            jobs.append(job)
+            break
+
+        return jobs
+
+    def scrape_jobs(self):
+        job_list = self.scrape_job_links(self.company.jobs_page_url)
+        self.prune_unlisted_jobs(job_list)
+        new_jobs = self.new_job_listings(job_list)
+
+        for job in new_jobs:
+            self.br.open(job.url)
+
+            s = soupify(self.br.response().read())
+            d = s.find('div', id='resumator-body')
+            v = d.find('div', id='resumator-content-right-wrapper')
+            v.extract()
+
+            job.desc = get_all_text(d)
+            job.save()
+
 def get_scraper():
-    return ResumatorJobScraper(COMPANY)
+    return ClearChoiceJobScraper()
 
 if __name__ == '__main__':
     job_scraper = get_scraper()
