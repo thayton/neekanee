588d1eef05fe24f1f4edc4ec39370ad2fcb02345
Todd Hayton <thayton@neekanee.com>
2013-09-30 07:07:48 -0400

    scanscout

--- neekanee/job_scrapers/plugins/com/fragmentid/scanscout.py
+++ neekanee/job_scrapers/plugins/com/fragmentid/scanscout.py
@@ -9,9 +9,6 @@ COMPANY = {
     'name': 'ScanScout',
     'hq': 'Boston, MA',
 
-    'contact': 'careers@scanscout.com',
-    'benefits': {'vacation': []},
-
     'home_page_url': 'http://www.scanscout.com',
     'jobs_page_url': 'http://www.scanscout.com/about_careers.php#careers-00006',
 
@@ -23,42 +20,13 @@ class ScanScoutJobScraper(JobScraper):
         super(ScanScoutJobScraper, self).__init__(COMPANY)
 
     def scrape_jobs(self):
-        self.br.open(self.company.jobs_page_url)
-
-        s = soupify(self.br.response().read())
-        r = re.compile(r'#careers-\d+')
-        v = { 'class': 'joblink', 'href': r }
-        d = s.find('div', id='content_wide_left')
-        d.extract()
-
+        # Acquired by Tremor Media
+        # http://paidcontent.org/2010/11/08/419-online-video-ad-net-tremor-media-acquires-scanscout/
         self.company.job_set.all().delete()
-
-        for a in d.findAll('a', attrs=v):
-            job = Job(company=self.company)
-            job.title = a.text
-            job.url = urlparse.urljoin(self.br.geturl(), a['href'])
-            job.location = self.company.location
-            job.desc = ''
-
-            x = d.find(attrs={'name' : a['href'][1:]})
-            x = x.next
-
-            l = x.findNext(text='Location:').next
-            l = self.parse_location(l)
-
-            if l is None:
-                continue
-
-            job.location = l
-
-            while x:
-                if hasattr(x, 'name') is False: 
-                    job.desc += x
-                elif x.name == 'br' and x.has_key('clear'):
-                    break
-                x = x.next
-
-            job.save()
 
 def get_scraper():
     return ScanScoutJobScraper()
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
