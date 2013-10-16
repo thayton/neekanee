cce196735e200568b56111b12e66cbabee7c4380
Todd Hayton <thayton@neekanee.com>
2013-10-16 09:35:07 -0400

    site gone

--- neekanee/job_scrapers/plugins/com/nolink/shreem.py
+++ neekanee/job_scrapers/plugins/com/nolink/shreem.py
@@ -9,9 +9,6 @@ COMPANY = {
     'name': 'Shreem',
     'hq': 'Sterling, VA',
 
-    'contact': 'recruiting@shreem.com',
-    'benefits': {'vacation': []},
-
     'home_page_url': 'http://www.shreem.com',
     'jobs_page_url': 'http://www.shreem.com/open_positions.htm',
 
@@ -23,20 +20,8 @@ class ShreemJobScraper(JobScraper):
         super(ShreemJobScraper, self).__init__(COMPANY)
 
     def scrape_jobs(self):
-        self.br.open(self.company.jobs_page_url)
-
-        s = soupify(self.br.response().read())
-        b = s.html.b
-        p = b.parent.parent
-
+        # Company has gone under / site is no longer there
         self.company.job_set.all().delete()
-
-        job = Job(company=self.company)
-        job.title = b.text
-        job.url = self.br.geturl()
-        job.location = self.company.location
-        job.desc = get_all_text(p)
-        job.save()
 
 def get_scraper():
     return ShreemJobScraper()
