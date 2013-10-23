411a3c0369c8b20030c602f6166cd88ab841caa3
Todd Hayton <thayton@neekanee.com>
2013-10-23 10:53:51 -0400

    defenders

---
+++ neekanee/job_scrapers/plugins/org/link/defenders.py
@@ -0,0 +1,59 @@
+import re, urlparse
+
+from neekanee.jobscrapers.jobscraper import JobScraper
+from neekanee.htmlparse.soupify import soupify, get_all_text, get_mailto
+from neekanee.urlutil import url_query_filter
+
+from neekanee_solr.models import *
+
+COMPANY = {
+    'name': 'Defenders of Wildlife',
+    'hq': 'Washington, DC',
+
+    'home_page_url': 'http://www.defenders.org',
+    'jobs_page_url': 'http://www.defenders.org/jobs',
+
+    'empcnt': [51,200]
+}
+
+class DefendersJobScraper(JobScraper):
+    def __init__(self):
+        super(DefendersJobScraper, self).__init__(COMPANY)
+
+    def scrape_job_links(self, url):
+        jobs = []
+
+        self.br.open(url)
+
+        s = soupify(self.br.response().read())
+        r = re.compile(r'^/jobs/[^/]+$')
+
+        for a in s.findAll('a', href=r):
+            job = Job(company=self.company)
+            job.title = a.text
+            job.url = urlparse.urljoin(self.br.geturl(), a['href'])
+            job.location = self.company.location
+            jobs.append(job)
+
+        return jobs
+
+    def scrape_jobs(self):
+        job_list = self.scrape_job_links(self.company.jobs_page_url)
+        self.prune_unlisted_jobs(job_list)
+        new_jobs = self.new_job_listings(job_list)
+
+        for job in new_jobs:
+            self.br.open(job.url)
+
+            s = soupify(self.br.response().read())
+            d = s.find('div', id='content')
+
+            job.desc = get_all_text(d)
+            job.save()
+
+def get_scraper():
+    return DefendersJobScraper()
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
