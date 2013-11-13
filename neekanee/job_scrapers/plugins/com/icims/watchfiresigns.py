8137dc479cd8f6f1b51c51a2861dbcdc08c4c58c
Todd Hayton <thayton@neekanee.com>
2013-11-13 09:26:11 -0500

    watchfiresigns

--- neekanee/job_scrapers/plugins/com/icims/watchfiresigns.py
+++ neekanee/job_scrapers/plugins/com/icims/watchfiresigns.py
@@ -1,11 +1,11 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'Watchfire Signs',
     'hq': 'Danville, IL',
 
     'home_page_url': 'http://www.watchfiresigns.com',
-    'jobs_page_url': 'https://jobs-watchfiresigns.icims.com/jobs/intro',
+    'jobs_page_url': 'https://jobs-watchfiresigns.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [201,500]
 }
@@ -13,9 +13,6 @@ COMPANY = {
 class WatchFireJobScraper(IcimsJobScraper):
     def __init__(self):
         super(WatchFireJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[-2].text)
 
 def get_scraper():
     return WatchFireJobScraper()
