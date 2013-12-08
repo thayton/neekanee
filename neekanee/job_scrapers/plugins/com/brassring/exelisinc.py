8ff53098e963f350dc324b0376e4e5102e038eba
Todd Hayton <thayton@neekanee.com>
2013-12-07 19:49:15 -0500

    exelisinc

--- neekanee/job_scrapers/plugins/com/brassring/exelisinc.py
+++ neekanee/job_scrapers/plugins/com/brassring/exelisinc.py
@@ -1,7 +1,5 @@
 import re
-
-from neekanee.jobscrapers.brassring.brassring import BrassringJobScraper
-from neekanee.htmlparse.soupify import soupify
+from neekanee.jobscrapers.kenexa.kenexa import KenexaJobScraper
 
 COMPANY = {
     'name': 'Exelis',
@@ -13,23 +11,13 @@ COMPANY = {
     'empcnt': [10001]
 }
 
-class ExelisIncJobScraper(BrassringJobScraper):
+class ExelisIncJobScraper(KenexaJobScraper):
     def __init__(self):
         super(ExelisIncJobScraper, self).__init__(COMPANY)
         self.soupify_search_form = True
 
-    def get_url_from_formtext(self, x):
-        s = soupify(x['OptionalReq'])
-        return s.a
-
-    def get_title_from_formtext(self, x):
-        return x['FORMTEXT26']
-
-    def get_location_from_formtext(self, x):
-        l = x['FORMTEXT11'] + ', ' + x['FORMTEXT22']
-        l = self.parse_location(l)
-
-        return l
+    def get_location_from_td(self, td):
+        return self.parse_location(td[3].text)
 
 def get_scraper():
     return ExelisIncJobScraper()
