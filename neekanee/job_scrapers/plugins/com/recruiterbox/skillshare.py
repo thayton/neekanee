ca569a42d10e2045cdf4efa7ede1f90be3ecb4b5
Todd Hayton <thayton@neekanee.com>
2013-12-09 11:35:31 -0500

    skillshare

---
+++ neekanee/job_scrapers/plugins/com/recruiterbox/skillshare.py
@@ -0,0 +1,67 @@
+import re, urlparse
+
+from neekanee.jobscrapers.jobscraper import JobScraper
+from neekanee.htmlparse.soupify import soupify, get_all_text
+
+from neekanee_solr.models import *
+
+COMPANY = {
+    'name': 'Skillshare',
+    'hq': 'New York City, NY',
+
+    'home_page_url': 'http://www.skillshare.com.',
+    'jobs_page_url': 'https://skillshare.recruiterbox.com',
+
+    'empcnt': [11,50]
+}
+
+class SkillShareJobScraper(JobScraper):
+    def __init__(self):
+        super(SkillShareJobScraper, self).__init__(COMPANY)
+
+    def scrape_job_links(self, url):
+        jobs = []
+
+        self.br.open(url)
+
+        s = soupify(self.br.response().read())
+        x = {'class': 'div_job_item_name'}
+        y = {'class': 'job_item_info_value'}
+
+        for d in s.findAll('div', attrs=x):
+            td = d.findParent('td')
+
+            p = td.find('span', attrs=y)
+            l = self.parse_location(p.text)
+
+            if not l:
+                continue
+
+            job = Job(company=self.company)
+            job.title = d.a.text
+            job.url = urlparse.urljoin(self.br.geturl(), d.a['href'])
+            job.location = l
+            jobs.append(job)
+
+        return jobs
+
+    def scrape_jobs(self):
+        job_list = self.scrape_job_links(self.company.jobs_page_url)
+        self.prune_unlisted_jobs(job_list)
+        new_jobs = self.new_job_listings(job_list)
+
+        for job in new_jobs:
+            self.br.open(job.url)
+
+            s = soupify(self.br.response().read())
+            d = s.find('div', id='div_main_section')
+
+            job.desc = get_all_text(d)
+            job.save()
+
+def get_scraper():
+    return SkillShareJobScraper()
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
