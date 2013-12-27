8d14b37f74fe554cf5a326d4666beae9d90bf213
Todd Hayton <thayton@neekanee.com>
2013-12-27 09:55:14 -0500

    rapid7

--- neekanee/job_scrapers/plugins/com/icims/rapid7.py
+++ neekanee/job_scrapers/plugins/com/icims/rapid7.py
@@ -1,24 +1,17 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'Rapid7',
     'hq': 'Boston, MA',
 
     'home_page_url': 'http://www.rapid7.com',
-    'jobs_page_url': 'https://careers-rapid7.icims.com/jobs/intro?hashed=0',
+    'jobs_page_url': 'https://careers-rapid7.icims.com/jobs/intro?hashed=0&in_iframe=1',
 
     'empcnt': [201,500]
 }
 
-class Rapid7JobScraper(IcimsJobScraper):
-    def __init__(self):
-        super(Rapid7JobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[2].text)
-
 def get_scraper():
-    return Rapid7JobScraper()
+    return IcimsJobScraper(COMPANY)
 
 if __name__ == '__main__':
     job_scraper = get_scraper()
