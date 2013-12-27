0d7069efefb9831d09b755542e1f504cfb63d6ec
Todd Hayton <thayton@neekanee.com>
2013-12-27 10:16:37 -0500

    eset

--- neekanee/job_scrapers/plugins/com/icims/eset.py
+++ neekanee/job_scrapers/plugins/com/icims/eset.py
@@ -1,24 +1,17 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'ESET',
     'hq': 'Bratislava, Slovak Republic',
 
     'home_page_url': 'http://www.eset.com',
-    'jobs_page_url': 'https://globalcareers-eset.icims.com/jobs/intro?hashed=0',
+    'jobs_page_url': 'https://globalcareers-eset.icims.com/jobs/intro?hashed=0&in_iframe=1',
 
     'empcnt': [1001,5000]
 }
 
-class EsetJobScraper(IcimsJobScraper):
-    def __init__(self):
-        super(EsetJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[-2].text)
-
 def get_scraper():
-    return EsetJobScraper()
+    return IcimsJobScraper(COMPANY)
 
 if __name__ == '__main__':
     job_scraper = get_scraper()
