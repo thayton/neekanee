f2700071b1cf4448b079e736cdc13ec449191ac5
Todd Hayton <thayton@neekanee.com>
2014-01-09 10:17:25 -0500

    aramark

--- neekanee/job_scrapers/plugins/com/icims/aramark.py
+++ neekanee/job_scrapers/plugins/com/icims/aramark.py
@@ -1,11 +1,11 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'Aramark',
     'hq': 'Philadelphia, PA',
 
     'home_page_url': 'http://www.aramark.com',
-    'jobs_page_url': 'https://allcareers-aramark.icims.com/jobs/intro?hashed=0',
+    'jobs_page_url': 'https://allcareers-aramark.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [10001]
 }
@@ -13,9 +13,6 @@ COMPANY = {
 class AramarkJobScraper(IcimsJobScraper):
     def __init__(self):
         super(AramarkJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[-3].text)
 
 def get_scraper():
     return AramarkJobScraper()
