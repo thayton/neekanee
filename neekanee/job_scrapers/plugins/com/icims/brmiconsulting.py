158b02d59782fa4f55747e8676e7609e58d5a180
Todd Hayton <thayton@neekanee.com>
2014-01-09 17:53:00 -0500

    brmiconsulting

--- neekanee/job_scrapers/plugins/com/icims/brmiconsulting.py
+++ neekanee/job_scrapers/plugins/com/icims/brmiconsulting.py
@@ -1,4 +1,4 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'BRMI Consulting',
@@ -9,7 +9,7 @@ COMPANY = {
     'contact': 'careers@brmiconsulting.com',
 
     'home_page_url': 'http://public.brmiconsulting.com',
-    'jobs_page_url': 'https://careers-brmi.icims.com/jobs/intro?hashed=0',
+    'jobs_page_url': 'https://careers-brmi.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [51,200]
 }
@@ -17,9 +17,6 @@ COMPANY = {
 class BrmiJobScraper(IcimsJobScraper):
     def __init__(self):
         super(BrmiJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[-2].text)
 
 def get_scraper():
     return BrmiJobScraper()
