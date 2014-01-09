4c91017dc024a27d4bf473535ba901ececd6fde2
Todd Hayton <thayton@neekanee.com>
2014-01-09 10:13:02 -0500

    akamai

--- neekanee/job_scrapers/plugins/com/link/akamai.py
+++ neekanee/job_scrapers/plugins/com/link/akamai.py
@@ -1,21 +1,72 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+import re, urlparse, mechanize
+
+from neekanee.jobscrapers.jobscraper import JobScraper
+from neekanee.htmlparse.soupify import soupify, get_all_text
+
+from neekanee_solr.models import *
 
 COMPANY = {
     'name': 'Akamai',
     'hq': 'Cambridge, MA',
 
     'home_page_url': 'http://www.akamai.com',
-    'jobs_page_url': 'https://jobs-akamai.icims.com/jobs/intro?hashed=0',
+    'jobs_page_url': 'http://jobs.akamai.com/search/',
 
     'empcnt': [1001,5000]
 }
 
-class AkamaiJobScraper(IcimsJobScraper):
+class AkamaiJobScraper(JobScraper):
     def __init__(self):
         super(AkamaiJobScraper, self).__init__(COMPANY)
 
-    def get_location_from_td(self, td):
-        return self.parse_location(td[-2].text)
+    def scrape_job_links(self, url):
+        jobs = []
+
+        self.br.open(url)
+
+        pageno = 2
+
+        while True:
+            s = soupify(self.br.response().read())
+            t = s.find('table', id='searchresults')
+            r = re.compile(r'^/job/\S+/\d+/$')
+
+            for a in t.findAll('a', href=r):
+                tr = a.findParent('tr')
+                td = tr.findAll('td')
+
+                l = self.parse_location(td[-2].text)
+                if not l:
+                    continue
+
+                job = Job(company=self.company)
+                job.title = a.text
+                job.url = urlparse.urljoin(self.br.geturl(), a['href'])
+                job.location = l
+                jobs.append(job)
+
+            try:
+                self.br.follow_link(self.br.find_link(text='Page %d' % pageno))
+                pageno += 1
+            except mechanize.LinkNotFoundError:
+                break
+
+        return jobs
+
+    def scrape_jobs(self):
+        job_list = self.scrape_job_links(self.company.jobs_page_url)
+        self.prune_unlisted_jobs(job_list)
+        new_jobs = self.new_job_listings(job_list)
+
+        for job in new_jobs:
+            self.br.open(job.url)
+
+            s = soupify(self.br.response().read())
+            x = {'class': 'jobDisplay'}
+            d = s.find('div', attrs=x)
+
+            job.desc = get_all_text(d)
+            job.save()
 
 def get_scraper():
     return AkamaiJobScraper()
