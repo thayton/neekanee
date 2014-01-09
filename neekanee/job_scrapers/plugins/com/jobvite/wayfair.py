871b8b85bede492c28b85ff3f41b2c3ef236e064
Todd Hayton <thayton@neekanee.com>
2014-01-09 07:32:28 -0500

    wayfair

--- neekanee/job_scrapers/plugins/com/jobvite/wayfair.py
+++ neekanee/job_scrapers/plugins/com/jobvite/wayfair.py
@@ -1,4 +1,4 @@
-import re, urlparse
+import re, urlparse, mechanize
 
 from neekanee.jobscrapers.jobscraper import JobScraper
 from neekanee.htmlparse.soupify import soupify, get_all_text
@@ -12,7 +12,7 @@ COMPANY = {
     'benefits': {'vacation': [(0,15)]},
 
     'home_page_url': 'http://www.wayfair.com',
-    'jobs_page_url': 'http://www.wayfair.com/careers#section=opportunities',
+    'jobs_page_url': 'http://hire.jobvite.com/Jobvite/Jobs.aspx?b=nbv7Ogwm',
 
     'empcnt': [501,1000]
 }
@@ -21,31 +21,72 @@ class WayFairJobScraper(JobScraper):
     def __init__(self):
         super(WayFairJobScraper, self).__init__(COMPANY)
 
+    def scrape_job_links(self, url):
+        jobs = []
+
+        self.br.open(url)
+
+        while True:
+            s = soupify(self.br.response().read())
+            r = re.compile(r'Jobvite/Job\.aspx\?b=')
+
+            for a in s.findAll('a', href=r):
+                if a.parent.name != 'b':
+                    continue
+
+                tr = a.findParent('tr')
+                td = tr.findAll('td')
+
+                l = td[0].findAll('span')[1]
+                l = self.parse_location(l.text)
+
+                if not l:
+                    continue
+
+                job = Job(company=self.company)
+                job.title = a.text
+                job.url = urlparse.urljoin(self.br.geturl(), a['href'])
+                job.location = l
+                jobs.append(job)
+
+            f = lambda x: x.name == 'a' and x.text == 'Next'
+            a = s.find(f)
+
+            if not a:
+                break
+
+            r = re.compile(r"__doPostBack\('([^']+)','([^']+)")
+            m = re.search(r, a['href'])
+
+            self.br.select_form('Form1')
+
+            ctl = self.br.form.find_control('Submit1')
+            self.br.form.controls.remove(ctl)
+
+            self.br.form.new_control('hidden', '__EVENTTARGET',   {'value': m.group(1)})
+            self.br.form.new_control('hidden', '__EVENTARGUMENT', {'value': m.group(2)})
+            self.br.form.fixup()
+            self.br.submit()
+
+        return jobs
+
     def scrape_jobs(self):
-        self.br.open(self.company.jobs_page_url)
+        job_list = self.scrape_job_links(self.company.jobs_page_url)
+        self.prune_unlisted_jobs(job_list)
+        new_jobs = self.new_job_listings(job_list)
 
-        s = soupify(self.br.response().read())
-        x = {'class': 'jobRow'}
+        for job in new_jobs:
+            self.br.open(job.url)
 
-        self.company.job_set.all().delete()
+            s = soupify(self.br.response().read())
+            f = s.find('form', attrs={'name': 'Form1'})
 
-        for d in s.findAll('div', attrs=x):
-            v = {'class': 'jobLocation'}
-            p = d.find('span', attrs=v)
-            l = self.parse_location(p.text)
-
-            if l is None:
-                continue
-
-            job = Job(company=self.company)
-            job.title = d.a.text
-            job.url = urlparse.urljoin(self.br.geturl(), '#job=%s' % d.a['data-id'])
-            job.location = l
-
-            v = s.find('div', id=d.a['data-id'])
-
-            job.desc = get_all_text(v)
+            job.desc = get_all_text(f)
             job.save()
 
 def get_scraper():
     return WayFairJobScraper()
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
