344d5a1e221d8e81d5dee52a559af21ea6e14c8e
Todd Hayton <thayton@neekanee.com>
2014-01-10 11:25:41 -0500

    datadirectnet

--- neekanee/job_scrapers/plugins/com/icims/datadirectnet.py
+++ neekanee/job_scrapers/plugins/com/icims/datadirectnet.py
@@ -1,11 +1,11 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'DataDirect Networks',
     'hq': 'Chatsworth, CA',
 
     'home_page_url': 'http://www.ddn.com',
-    'jobs_page_url': 'https://careers-ddn.icims.com/jobs/intro?hashed=0',
+    'jobs_page_url': 'https://careers-ddn.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [201,500]
 }
@@ -13,9 +13,6 @@ COMPANY = {
 class DataDirectNetJobScraper(IcimsJobScraper):
     def __init__(self):
         super(DataDirectNetJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[-1].text)
 
 def get_scraper():
     return DataDirectNetJobScraper()
