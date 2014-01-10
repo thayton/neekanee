bd6a8c457be1b1d94b8cdda4503a95743d46a57c
Todd Hayton <thayton@neekanee.com>
2014-01-10 11:45:09 -0500

    elbitsystems-us

--- neekanee/job_scrapers/plugins/com/icims/elbitsystems-us.py
+++ neekanee/job_scrapers/plugins/com/icims/elbitsystems-us.py
@@ -1,4 +1,4 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'Elbit Systems of America',
@@ -7,7 +7,7 @@ COMPANY = {
     'ats': 'icims',
 
     'home_page_url': 'http://www.elbitsystems-us.com',
-    'jobs_page_url': 'https://jobs-esa.icims.com/jobs/intro',
+    'jobs_page_url': 'https://jobs-esa.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [1001,5000],
 }
@@ -15,10 +15,6 @@ COMPANY = {
 class ElbitSystemsUsJobScraper(IcimsJobScraper):
     def __init__(self):
         super(ElbitSystemsUsJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        l = td[-2].text
-        return self.parse_location(l)
 
 def get_scraper():
     return ElbitSystemsUsJobScraper()
