d3dff3958ac74654484b1c8d62b77e108403fb62
Todd Hayton <thayton@neekanee.com>
2014-01-15 09:44:58 -0500

    unleashbts

--- neekanee/job_scrapers/plugins/com/link/unleashbts.py
+++ neekanee/job_scrapers/plugins/com/link/unleashbts.py
@@ -1,4 +1,4 @@
-import re, urlparse
+import re, urlparse, mechanize
 
 from neekanee.jobscrapers.jobscraper import JobScraper
 from neekanee.htmlparse.soupify import soupify, get_all_text
@@ -10,7 +10,7 @@ COMPANY = {
     'hq': 'Columbia, MD',
 
     'home_page_url': 'http://www.unleashbts.com',
-    'jobs_page_url': 'http://www.unleashbts.com/careers',
+    'jobs_page_url': 'http://www.unleashbts.com/category/careers/',
 
     'empcnt': [11,50]
 }
@@ -19,26 +19,56 @@ class UnleashBtsJobScraper(JobScraper):
     def __init__(self):
         super(UnleashBtsJobScraper, self).__init__(COMPANY)
 
+    def scrape_job_links(self, url):
+        jobs = []
+
+        pageno = 2
+
+        self.br.open(url)
+
+        while True:
+            s = soupify(self.br.response().read())
+            x = {'class': 'post-title'}
+        
+            for h2 in s.findAll('h2', attrs=x):
+                job = Job(company=self.company)
+                job.title = h2.a.text
+                job.url = urlparse.urljoin(self.br.geturl(), h2.a['href'])
+                job.location = self.company.location
+                jobs.append(job)
+
+            try:
+                self.br.follow_link(self.br.find_link(text='%d' % pageno))
+                pageno += 1
+            except mechanize.LinkNotFoundError:
+                break
+
+        return jobs
+
     def scrape_jobs(self):
-        self.br.open(self.company.jobs_page_url)
+        job_list = self.scrape_job_links(self.company.jobs_page_url)
+        self.prune_unlisted_jobs(job_list)
+        new_jobs = self.new_job_listings(job_list)
 
-        s = soupify(self.br.response().read())
-        r = re.compile(r'row')
-        x = {'class': r, 'title': True, 'location': True}
+        for job in new_jobs:
+            self.br.open(job.url)
 
-        for id in ['full_time', 'internships']:
-            d = s.find('div', id=id)
-            for v in d.findAll('div', attrs=x):
-                l = self.parse_location(v['location'])
+            s = soupify(self.br.response().read())
+            x = {'class': 'article'}
+            a = s.find('article', attrs=x)
+            t = a.find(text=re.compile(r'^Location:'))
+
+            if t:
+                l = t.split('Location:')[1]
+                l = self.parse_location(l)
+
                 if not l:
                     continue
+                else:
+                    job.location = l
 
-                job = Job(company=self.company)
-                job.title = v.h2.text
-                job.url = self.br.geturl()
-                job.location = l
-                job.desc = get_all_text(v)
-                job.save()
+            job.desc = get_all_text(a)
+            job.save()
 
 def get_scraper():
     return UnleashBtsJobScraper()
