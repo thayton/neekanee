e55e5de352ace2dfd321c8970f4ef89f43f3c8d3
Todd Hayton <thayton@neekanee.com>
2014-01-15 11:08:55 -0500

    peopleperhour

---
+++ neekanee/job_scrapers/plugins/com/jobscore/peopleperhour.py
@@ -0,0 +1,18 @@
+from neekanee.jobscrapers.jobscore.jobscore import JobScoreJobScraper
+
+COMPANY = {
+    'name': 'PeoplePerHour.com',
+    'hq': 'London, England',
+
+    'home_page_url': 'http://www.peopleperhour.com',
+    'jobs_page_url': 'http://www.jobscore.com/jobs/peopleperhourcom/',
+
+    'empcnt': [11,50]
+}
+
+def get_scraper():
+    return JobScoreJobScraper(COMPANY)
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
