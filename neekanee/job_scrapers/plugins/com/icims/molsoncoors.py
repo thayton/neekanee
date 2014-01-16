c4b524c50fba3f4dc8feb0a6339e841e64a6e3b5
Todd Hayton <thayton@neekanee.com>
2014-01-16 11:10:09 -0500

    molsoncoors

--- neekanee/job_scrapers/plugins/com/icims/molsoncoors.py
+++ neekanee/job_scrapers/plugins/com/icims/molsoncoors.py
@@ -1,11 +1,11 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'Molson Coors',
     'hq': 'Denver, CO',
 
     'home_page_url': 'http://www.molsoncoors.com',
-    'jobs_page_url': 'https://ca-extenglish-molsoncoors.icims.com/jobs/intro',
+    'jobs_page_url': 'https://ca-extenglish-molsoncoors.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [1001,5000]
 }
@@ -13,9 +13,6 @@ COMPANY = {
 class MolsonCoorsJobScraper(IcimsJobScraper):
     def __init__(self):
         super(MolsonCoorsJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[2].text)
 
 def get_scraper():
     return MolsonCoorsJobScraper()
