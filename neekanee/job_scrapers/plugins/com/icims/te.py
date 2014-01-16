7a99cf2ef001a13e24e46348eff76e6250917282
Todd Hayton <thayton@neekanee.com>
2014-01-16 10:04:15 -0500

    te

--- neekanee/job_scrapers/plugins/com/icims/te.py
+++ neekanee/job_scrapers/plugins/com/icims/te.py
@@ -1,11 +1,11 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'TE Connectivity',
     'hq': 'Berwyn, PA',
 
     'home_page_url': 'http://www.te.com',
-    'jobs_page_url': 'https://jobs-tycoelectronics.icims.com/jobs/intro',
+    'jobs_page_url': 'https://jobs-tycoelectronics.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [10001]
 }
@@ -13,9 +13,6 @@ COMPANY = {
 class TeJobScraper(IcimsJobScraper):
     def __init__(self):
         super(TeJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[2].text)
 
 def get_scraper():
     return TeJobScraper()
