69787b2fa33efeff95b08b725e862deda0b106c1
Todd Hayton <thayton@neekanee.com>
2014-01-16 10:11:17 -0500

    siriusxm

--- neekanee/job_scrapers/plugins/com/icims/siriusxm.py
+++ neekanee/job_scrapers/plugins/com/icims/siriusxm.py
@@ -1,4 +1,4 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'SiriusXM',
@@ -7,7 +7,7 @@ COMPANY = {
     'ats': 'icims',
 
     'home_page_url': 'http://www.siriusxm.com',
-    'jobs_page_url': 'https://careers-siriusxm.icims.com/jobs/intro',
+    'jobs_page_url': 'https://careers-siriusxm.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [1001,5000]
 }
@@ -15,9 +15,6 @@ COMPANY = {
 class SiriusXmJobScraper(IcimsJobScraper):
     def __init__(self):
         super(SiriusXmJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[-1].text)
 
 def get_scraper():
     return SiriusXmJobScraper()
