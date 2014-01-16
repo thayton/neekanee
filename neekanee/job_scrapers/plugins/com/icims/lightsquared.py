7268d4313db05d556c897a86e7ed7d60113d7470
Todd Hayton <thayton@neekanee.com>
2014-01-16 11:36:25 -0500

    lightsquared

--- neekanee/job_scrapers/plugins/com/icims/lightsquared.py
+++ neekanee/job_scrapers/plugins/com/icims/lightsquared.py
@@ -1,11 +1,11 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'LightSquared',
     'hq': 'Reston, VA',
 
     'home_page_url': 'http://www.lightsquared.com',
-    'jobs_page_url': 'https://jobs-lightsquared.icims.com/jobs/intro',
+    'jobs_page_url': 'https://jobs-lightsquared.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [201,500]
 }
@@ -13,9 +13,6 @@ COMPANY = {
 class LightSquaredJobScraper(IcimsJobScraper):
     def __init__(self):
         super(LightSquaredJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[-2].text)
 
 def get_scraper():
     return LightSquaredJobScraper()
