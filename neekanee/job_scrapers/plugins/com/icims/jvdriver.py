3ba3c1aec9f390b891c704648bc652782395cfc3
Todd Hayton <thayton@neekanee.com>
2014-01-16 11:43:07 -0500

    jvdriver

--- neekanee/job_scrapers/plugins/com/icims/jvdriver.py
+++ neekanee/job_scrapers/plugins/com/icims/jvdriver.py
@@ -1,11 +1,11 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'JV Driver Projects',
     'hq': 'Leduc, Canada',
 
     'home_page_url': 'http://www.jvdriver.com',
-    'jobs_page_url': 'https://careers-jvdriver.icims.com/jobs/intro?hashed=0',
+    'jobs_page_url': 'https://careers-jvdriver.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [1001,5000]
 }
@@ -13,9 +13,6 @@ COMPANY = {
 class JvDriverJobScraper(IcimsJobScraper):
     def __init__(self):
         super(JvDriverJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[-1].text)
 
 def get_scraper():
     return JvDriverJobScraper()
