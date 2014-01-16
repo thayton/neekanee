e0cf0f74206b529850af7866a0f6fcee5479ce8a
Todd Hayton <thayton@neekanee.com>
2014-01-16 11:58:42 -0500

    jcrew

--- neekanee/job_scrapers/plugins/com/icims/jcrew.py
+++ neekanee/job_scrapers/plugins/com/icims/jcrew.py
@@ -1,11 +1,11 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'J. Crew',
     'hq': 'New York, NY',
 
     'home_page_url': 'http://www.jcrew.com',
-    'jobs_page_url': 'https://jobs-jcrew.icims.com/jobs/intro?hashed=0',
+    'jobs_page_url': 'https://jobs-jcrew.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [5001,10000]
 }
@@ -13,9 +13,6 @@ COMPANY = {
 class JCrewJobScraper(IcimsJobScraper):
     def __init__(self):
         super(JCrewJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[1].text)
 
 def get_scraper():
     return JCrewJobScraper()
