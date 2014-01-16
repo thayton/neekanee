764c785dd5029e259d663bd691b3f8a3d9288820
Todd Hayton <thayton@neekanee.com>
2014-01-16 10:00:08 -0500

    reardencommerce

--- neekanee/job_scrapers/plugins/com/icims/reardencommerce.py
+++ neekanee/job_scrapers/plugins/com/icims/reardencommerce.py
@@ -1,11 +1,11 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'Rearden Commerce',
     'hq': 'Foster City, CA',
 
     'home_page_url': 'http://www.reardencommerce.com/',
-    'jobs_page_url': 'https://careers-reardencommerce.icims.com/jobs/intro',
+    'jobs_page_url': 'https://careers-reardencommerce.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [501,1000]
 }
@@ -13,12 +13,6 @@ COMPANY = {
 class ReardenCommerceJobScraper(IcimsJobScraper):
     def __init__(self):
         super(ReardenCommerceJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        x = {'itemprop': 'address'}
-        p = td[-1].find('span', attrs=x)
-        l = self.parse_location(p.text)
-        return l
 
 def get_scraper():
     return ReardenCommerceJobScraper()
