842a0f0db4acb44e4804917ac721dd2eedc57745
Todd Hayton <thayton@neekanee.com>
2014-01-31 11:13:22 -0500

    uvu

--- neekanee/job_scrapers/plugins/edu/peopleadmin/uvu.py
+++ neekanee/job_scrapers/plugins/edu/peopleadmin/uvu.py
@@ -1,14 +1,18 @@
-from neekanee.jobscrapers.peopleadmin.peopleadmin2 import PeopleAdminJobScraper
+from neekanee.jobscrapers.peopleadmin.peopleadmin3 import PeopleAdminJobScraper
 
 COMPANY = {
     'name': 'Utah Valley University',
     'hq': 'Orem, UT',
 
     'home_page_url': 'http://www.uvu.edu',
-    'jobs_page_url': 'https://www.uvu.jobs/applicants/jsp/shared/index.jsp',
+    'jobs_page_url': 'https://www.uvu.jobs',
 
     'empcnt': [1001,5000]
 }
 
 def get_scraper():
     return PeopleAdminJobScraper(COMPANY)
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
