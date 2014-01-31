7598a1c9b2932fe7d40b83e7e53c4c3895df609a
Todd Hayton <thayton@neekanee.com>
2014-01-31 05:46:29 -0500

    usc

--- neekanee/job_scrapers/plugins/edu/peopleadmin/usc.py
+++ neekanee/job_scrapers/plugins/edu/peopleadmin/usc.py
@@ -1,16 +1,18 @@
-from neekanee.jobscrapers.peopleadmin.peopleadmin2 import PeopleAdminJobScraper
+from neekanee.jobscrapers.peopleadmin.peopleadmin3 import PeopleAdminJobScraper
 
 COMPANY = {
     'name': 'University of Southern California',
     'hq': 'Los Angeles, CA',
 
     'home_page_url': 'http://www.usc.edu',
-    'jobs_page_url': 'https://jobs.usc.edu/applicants/jsp/shared/index.jsp',
-
-    'gctw_chronicle': True,
+    'jobs_page_url': 'https://jobs.usc.edu',
 
     'empcnt': [10001]
 }
 
 def get_scraper():
     return PeopleAdminJobScraper(COMPANY)
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
