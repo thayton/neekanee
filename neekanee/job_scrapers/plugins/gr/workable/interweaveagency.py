8ac9cb55768f900c630277c409e8ce55274d0bdc
Todd Hayton <thayton@neekanee.com>
2014-02-05 07:28:45 -0500

    interweaveagency

---
+++ neekanee/job_scrapers/plugins/gr/workable/interweaveagency.py
@@ -0,0 +1,18 @@
+from neekanee.jobscrapers.workable.workable import WorkableJobScraper
+
+COMPANY = {
+    'name': 'Interweave',
+    'hq': 'Athens, Greece',
+
+    'home_page_url': 'http://www.interweaveagency.gr',
+    'jobs_page_url': 'http://interweave.workable.com',
+
+    'empcnt': [11,50]
+}
+
+def get_scraper():
+    return WorkableJobScraper(COMPANY)
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
