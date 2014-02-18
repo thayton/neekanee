e1be1bb1cc14f4aaf9209773c464010c796cb2f9
Todd Hayton <thayton@neekanee.com>
2014-02-18 15:13:01 -0500

    wsu

--- neekanee/job_scrapers/plugins/edu/peopleadmin/wsu.py
+++ neekanee/job_scrapers/plugins/edu/peopleadmin/wsu.py
@@ -1,14 +1,18 @@
-from neekanee.jobscrapers.peopleadmin.PeopleAdminATS import PeopleAdminJobScraper
+from neekanee.jobscrapers.peopleadmin.peopleadmin3 import PeopleAdminJobScraper
 
 COMPANY = {
     'name': 'Washington State University',
     'hq': 'Pullman, WA',
 
     'home_page_url': 'http://www.wsu.edu',
-    'jobs_page_url': 'https://www.wsujobs.com/applicants/jsp/shared/Welcome_css.jsp',
+    'jobs_page_url': 'https://www.wsujobs.com',
 
     'empcnt': [1001,5000]
 }
 
 def get_scraper():
     return PeopleAdminJobScraper(COMPANY)
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
