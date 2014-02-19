cf6311352c7303cc253d0224f04f04b550c02d43
Todd Hayton <thayton@neekanee.com>
2014-02-18 19:46:00 -0500

    enterasys

--- neekanee/job_scrapers/plugins/com/force/enterasys.py
+++ neekanee/job_scrapers/plugins/com/force/enterasys.py
@@ -19,46 +19,10 @@ class EnterasysJobScraper(JobScraper):
     def __init__(self):
         super(EnterasysJobScraper, self).__init__(COMPANY)
 
-    def scrape_job_links(self, url):
-        jobs = []
-
-        # XXX TODO handle pagination
-        self.br.open(url)
-
-        r = re.compile(r'/careers/ts2__JobDetails\?jobId=\w+')
-        s = soupify(self.br.response().read())
-
-        for a in s.findAll('a', href=r):
-            tr = a.findParent('tr')
-            td = tr.findAll('td')
-
-            l = self.parse_location(td[-1].text)
-            if not l:
-                continue
-
-            job = Job(company=self.company)
-            job.title = a.text
-            job.location = l
-            job.url = urlparse.urljoin(self.br.geturl(), a['href'])
-            job.url = urlutil.url_query_filter(job.url, 'jobId')
-            jobs.append(job)
-
-        return jobs
-
     def scrape_jobs(self):
-        job_list = self.scrape_job_links(self.company.jobs_page_url)
-        self.prune_unlisted_jobs(job_list)
-        new_jobs = self.new_job_listings(job_list)
-
-        for job in new_jobs:
-            self.br.open(job.url)
-
-            s = soupify(self.br.response().read())
-            d = s.find('div', id='content')
-            d = d.div
-
-            job.desc = get_all_text(d)
-            job.save()
+        # XXX 
+        # Enterasys is now extremem networks
+        self.company.job_set.all().delete()
 
 def get_scraper():
     return EnterasysJobScraper()
