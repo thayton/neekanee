2c3a4398a097e3e8030e6f1a6532934858c7f4f4
Todd Hayton <thayton@neekanee.com>
2014-02-24 11:32:36 -0500

    sait

--- neekanee/job_scrapers/plugins/ca/icims/sait.py
+++ neekanee/job_scrapers/plugins/ca/icims/sait.py
@@ -1,11 +1,11 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'SAIT Polytechnic',
     'hq': 'Calgary, Canada',
 
     'home_page_url': 'http://www.sait.ca',
-    'jobs_page_url': 'https://careers-sait.icims.com/jobs/intro',
+    'jobs_page_url': 'https://careers-sait.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [1001,5000]
 }
@@ -13,9 +13,6 @@ COMPANY = {
 class SaitJobScraper(IcimsJobScraper):
     def __init__(self):
         super(SaitJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[-2].text)
 
 def get_scraper():
     return SaitJobScraper()
