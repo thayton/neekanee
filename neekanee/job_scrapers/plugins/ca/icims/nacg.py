53d73cb3b0dc8a6d7a1041386f3e3835b8aa6fb7
Todd Hayton <thayton@neekanee.com>
2014-02-26 09:40:39 -0500

    nacg

--- neekanee/job_scrapers/plugins/ca/icims/nacg.py
+++ neekanee/job_scrapers/plugins/ca/icims/nacg.py
@@ -1,11 +1,12 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+import re
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'North American Construction Group',
     'hq': 'Acheson, Canada',
 
     'home_page_url': 'http://www.nacg.ca',
-    'jobs_page_url': 'http://careers-nacg.icims.com/',
+    'jobs_page_url': 'http://careers-nacg.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [1001,5000]
 }
@@ -14,8 +15,12 @@ class NacgJobScraper(IcimsJobScraper):
     def __init__(self):
         super(NacgJobScraper, self).__init__(COMPANY)
 
-    def get_location_from_td(self, td):
-        return self.parse_location(td[-2].text)
+    def get_location_from_div(self, div):
+        r = re.compile(r'^Job City:')
+        t = div.find(text=r)
+        d = t.findParent('div')
+        l = self.parse_location(d.contents[-1])
+        return l
 
 def get_scraper():
     return NacgJobScraper()
