0e6056f2a5d2b1ddd5c52a57f86f34b8f7acab35
Todd Hayton <thayton@neekanee.com>
2014-02-26 10:10:16 -0500

    fidelissecurity

--- neekanee/job_scrapers/plugins/com/icims/fidelissecurity.py
+++ neekanee/job_scrapers/plugins/com/icims/fidelissecurity.py
@@ -1,11 +1,11 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'Fidelis Security Systems',
     'hq': 'Bethesda, MD',
 
     'home_page_url': 'http://www.fidelissecurity.com',
-    'jobs_page_url': 'https://careers-fidelissecuritysystems.icims.com/jobs/intro',
+    'jobs_page_url': 'https://careers-fidelissecuritysystems.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [11,50]
 }
@@ -13,9 +13,6 @@ COMPANY = {
 class FidelisSecurityJobScraper(IcimsJobScraper):
     def __init__(self):
         super(FidelisSecurityJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[2].text)
 
 def get_scraper():
     return FidelisSecurityJobScraper()
