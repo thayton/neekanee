3d7d3299957bff5f24c067b520efa2b3842c76f5
Todd Hayton <thayton@neekanee.com>
2014-03-03 08:15:16 -0500

    gandermountain

--- neekanee/job_scrapers/plugins/com/icims/gandermountain.py
+++ neekanee/job_scrapers/plugins/com/icims/gandermountain.py
@@ -1,11 +1,11 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'Gander Mountain',
     'hq': 'St Paul, MN',
 
     'home_page_url': 'https://www.gandermountain.com/',
-    'jobs_page_url': 'https://jobs-gandermountain.icims.com/jobs/intro',
+    'jobs_page_url': 'https://jobs-gandermountain.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [5001,10000],
 }
@@ -13,10 +13,6 @@ COMPANY = {
 class GanderMountainJobScraper(IcimsJobScraper):
     def __init__(self):
         super(GanderMountainJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        l = td[2].text
-        return self.parse_location(l)
 
 def get_scraper():
     return GanderMountainJobScraper()
