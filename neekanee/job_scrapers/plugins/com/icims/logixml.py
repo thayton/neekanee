d1f9542c6a4c6ffe013b927fa88e58f95a3aa285
Todd Hayton <thayton@neekanee.com>
2014-03-03 08:23:32 -0500

    logixml

--- neekanee/job_scrapers/plugins/com/icims/logixml.py
+++ neekanee/job_scrapers/plugins/com/icims/logixml.py
@@ -1,4 +1,4 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'LogiXML',
@@ -7,7 +7,7 @@ COMPANY = {
     'contact': 'Jobs@logixml.com',
 
     'home_page_url': 'http://www.logixml.com',
-    'jobs_page_url': 'https://careers-logixml.icims.com/jobs/intro?hashed=0',
+    'jobs_page_url': 'https://careers-logixml.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [51,200]
 }
@@ -15,9 +15,6 @@ COMPANY = {
 class LogiXmlJobScraper(IcimsJobScraper):
     def __init__(self):
         super(LogiXmlJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[-2].text)
 
 def get_scraper():
     return LogiXmlJobScraper()
