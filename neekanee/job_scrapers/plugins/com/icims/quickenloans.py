eb40aaf62ab49668f3060b4b6e4630f101e0e309
Todd Hayton <thayton@neekanee.com>
2014-03-03 08:12:13 -0500

    quickenloans

--- neekanee/job_scrapers/plugins/com/icims/quickenloans.py
+++ neekanee/job_scrapers/plugins/com/icims/quickenloans.py
@@ -1,11 +1,11 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'Quicken Loans',
     'hq': 'Detroit, MI',
 
     'home_page_url': 'http://www.quickenloans.com',
-    'jobs_page_url': 'https://careers-quickenloans.icims.com/jobs/intro',
+    'jobs_page_url': 'https://careers-quickenloans.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [1001,5000]
 }
@@ -13,9 +13,6 @@ COMPANY = {
 class QuickenLoansJobScraper(IcimsJobScraper):
     def __init__(self):
         super(QuickenLoansJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[-1].text)
 
 def get_scraper():
     return QuickenLoansJobScraper()
