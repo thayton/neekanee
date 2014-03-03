6134635403098cf673bda8855b0b19affda670ca
Todd Hayton <thayton@neekanee.com>
2014-03-03 08:27:37 -0500

    oasispetroleum

--- neekanee/job_scrapers/plugins/com/icims/oasispetroleum.py
+++ neekanee/job_scrapers/plugins/com/icims/oasispetroleum.py
@@ -1,11 +1,11 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'Oasis Petroleum',
     'hq': 'Houston, TX',
 
     'home_page_url': 'http://www.oasispetroleum.com',
-    'jobs_page_url': 'https://jobs-oasispetroleum.icims.com/jobs/intro?hashed=0',
+    'jobs_page_url': 'https://jobs-oasispetroleum.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [201,500]
 }
@@ -13,9 +13,6 @@ COMPANY = {
 class OasisPetroleumJobScraper(IcimsJobScraper):
     def __init__(self):
         super(OasisPetroleumJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[-1].text)
 
 def get_scraper():
     return OasisPetroleumJobScraper()
