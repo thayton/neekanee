425facd3e3b4c15194881eb84688fdb675c507e1
Todd Hayton <thayton@neekanee.com>
2014-03-03 08:45:29 -0500

    veeco

--- neekanee/job_scrapers/plugins/com/icims/veeco.py
+++ neekanee/job_scrapers/plugins/com/icims/veeco.py
@@ -1,11 +1,11 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'Veeco',
     'hq': 'Plainview, NY',
 
     'home_page_url': 'http://www.veeco.com',
-    'jobs_page_url': 'https://hub-veeco.icims.com',
+    'jobs_page_url': 'https://hub-veeco.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [1001,5000]
 }
@@ -13,9 +13,6 @@ COMPANY = {
 class VeecoJobScraper(IcimsJobScraper):
     def __init__(self):
         super(VeecoJobScraper, self).__init__(COMPANY)
-
-    def get_location_from_td(self, td):
-        return self.parse_location(td[-1].text)
 
 def get_scraper():
     return VeecoJobScraper()
