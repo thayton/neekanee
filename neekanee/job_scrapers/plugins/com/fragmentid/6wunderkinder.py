fd1519ea598e4f53c5286ed62a60894fb1a81e92
Todd Hayton <thayton@neekanee.com>
2014-03-10 11:38:32 -0400

    6wunderkinder

--- neekanee/job_scrapers/plugins/com/fragmentid/6wunderkinder.py
+++ neekanee/job_scrapers/plugins/com/fragmentid/6wunderkinder.py
@@ -10,7 +10,7 @@ COMPANY = {
     'hq': 'Berlin, Germany',
 
     'home_page_url': 'http://www.6wunderkinder.com',
-    'jobs_page_url': 'http://www.6wunderkinder.com/jobs/',
+    'jobs_page_url': 'http://www.6wunderkinder.com/en/jobs',
 
     'empcnt': [11,50]
 }
@@ -18,6 +18,8 @@ COMPANY = {
 class SixWunderkinderJobScraper(JobScraper):
     def __init__(self):
         super(SixWunderkinderJobScraper, self).__init__(COMPANY)
+        self.br.addheaders = [('User-agent', 
+                               'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7')]
 
     def scrape_jobs(self):
         self.br.open(self.company.jobs_page_url)
