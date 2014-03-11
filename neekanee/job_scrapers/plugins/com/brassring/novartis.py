d44678051df4c4cc224186a7b49d2bad6230c5d3
Todd Hayton <thayton@neekanee.com>
2014-03-11 11:43:40 -0400

    now brassring

--- neekanee/job_scrapers/plugins/com/brassring/novartis.py
+++ neekanee/job_scrapers/plugins/com/brassring/novartis.py
@@ -1,43 +1,40 @@
-from neekanee.jobscrapers.kenexa.kenexa import KenexaJobScraper
+import re
+
+from neekanee.jobscrapers.brassring.brassring import BrassringJobScraper
+from neekanee.htmlparse.soupify import soupify
 
 COMPANY = {
     'name': 'Novartis',
     'hq': 'Cambridge, MA',
 
-    'ats': 'Kenexa',
-
     'home_page_url': 'http://www.novartis.com',
-    'jobs_page_url': 'https://sjobs.brassring.com/2057/ASP/TG/cim_home.asp?partnerid=13617&siteid=5260',
+    'jobs_page_url': 'https://sjobs.brassring.com/tgwebhost/home.aspx?partnerid=13617&siteid=5260',
 
     'empcnt': [10001]
 }
 
-class NovartisJobScraper(KenexaJobScraper):
+class NovartisJobScraper(BrassringJobScraper):
     def __init__(self):
         super(NovartisJobScraper, self).__init__(COMPANY)
         self.soupify_search_form = True
 
-    def update_frmAgent_form(self):
-        #
-        # Set field to limit results to USA only and allow all languages
-        #
-        self.br.form.new_control('hidden', 'Question20146__FORMTEXT27',   {'value':''})
-        self.br.form.new_control('hidden', 'GTGLanguageList',             {'value':''})
-        self.br.form.fixup()
+    def get_url_from_formtext(self, x):
+        s = soupify(x['JobTitle'])
+        return s.a
 
-        self.br.form.set_all_readonly(False)
-        self.br.form['Question20146__FORMTEXT27'] = 'AnswerName&=|USA=X|%%%AnswerValue&=|US=X|%%%GDEAnswerValue&=|=X|???'
-        self.br.form['GTGLanguageList'] = 'All'
+    def get_title_from_formtext(self, x):
+        s = soupify(x['JobTitle'])
+        return s.a.text
 
-    def get_title_from_td(self, td):
-        return td[2].text
+    def get_location_from_formtext(self, x):
+        l = x['FORMTEXT28'] + ', ' + x['FORMTEXT27']
+        l = self.parse_location(l)
 
-    def get_location_from_td(self, td):
-        y = td[4].text.strip() 
-        if y != 'USA':
-            return None
-
-        return self.parse_location(td[5].text)
+        return l
 
 def get_scraper():
     return NovartisJobScraper()
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
