e979eeba48c94fa24a9ba04134c4164fd7c2026b
Todd Hayton <thayton@neekanee.com>
2014-03-19 11:49:37 -0400

    netnumber

---
+++ neekanee/job_scrapers/plugins/com/link/netnumber.py
@@ -0,0 +1,60 @@
+import re, urlparse
+
+from neekanee.jobscrapers.jobscraper import JobScraper
+from neekanee.htmlparse.soupify import soupify, get_all_text, get_mailto
+
+from neekanee_solr.models import *
+
+COMPANY = {
+    'name': 'NetNumber',
+    'hq': 'Lowell, MA',
+
+    'home_page_url': 'http://www.netnumber.com',
+    'jobs_page_url': 'http://netnumber.com/about/join-us/',
+
+    'empcnt': [11,50]
+}
+
+class NetNumberJobScraper(JobScraper):
+    def __init__(self):
+        super(NetNumberJobScraper, self).__init__(COMPANY)
+
+    def scrape_job_links(self, url):
+        jobs = []
+
+        self.br.open(url)
+
+        s = soupify(self.br.response().read())
+        r = re.compile(r'^/\?page_id=\d+$')
+        f = lambda x: x.name == 'a' and re.search(r, x.get('href', '')) and x.b
+
+        for a in s.findAll(f):
+            job = Job(company=self.company)
+            job.title = a.text
+            job.url = urlparse.urljoin(url, a['href'])
+            job.location = self.company.location
+            jobs.append(job)
+
+        return jobs
+
+    def scrape_jobs(self):
+        job_list = self.scrape_job_links(self.company.jobs_page_url)
+        self.prune_unlisted_jobs(job_list)
+        new_jobs = self.new_job_listings(job_list)
+
+        for job in new_jobs:
+            self.br.open(job.url)
+
+            s = soupify(self.br.response().read())
+            r = re.compile(r'^post-\d+$')
+            d = s.find('div', id=r)
+
+            job.desc = get_all_text(d)
+            job.save()
+
+def get_scraper():
+    return NetNumberJobScraper()
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
