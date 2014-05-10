9d058e1e41921869216c15f4cf438372cc73ef30
Todd Hayton <thayton@neekanee.com>
2014-05-10 08:03:30 -0400

    enterprise

--- neekanee/job_scrapers/plugins/com/icims/enterprise.py
+++ neekanee/job_scrapers/plugins/com/icims/enterprise.py
@@ -1,4 +1,4 @@
-from neekanee.jobscrapers.icims.icims import IcimsJobScraper
+from neekanee.jobscrapers.icims.icims2 import IcimsJobScraper
 
 COMPANY = {
     'name': 'Enterprise Rent-A-Car',
@@ -7,7 +7,7 @@ COMPANY = {
     'ats': 'icims',
 
     'home_page_url': 'http://www.enterprise.com',
-    'jobs_page_url': 'https://us-erac.icims.com/jobs/search?ss=1',
+    'jobs_page_url': 'https://us-erac.icims.com/jobs/intro?in_iframe=1',
 
     'empcnt': [10001]
 }
@@ -16,8 +16,13 @@ class EnterpriseJobScraper(IcimsJobScraper):
     def __init__(self):
         super(EnterpriseJobScraper, self).__init__(COMPANY)
 
-    def get_location_from_td(self, td):
-        return self.parse_location(td[-2].text)
+    def get_location_from_div(self, div):
+        x = {'itemprop': 'address'}
+        d = div.find(attrs=x)
+        l = d.text
+        d = div.findAll('div')
+        l += '-' + d[-1].contents[-1].strip()
+        return self.parse_location(l)
 
 def get_scraper():
     return EnterpriseJobScraper()
