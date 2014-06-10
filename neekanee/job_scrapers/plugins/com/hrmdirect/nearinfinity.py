05985841fb1ec429a8991bf2f9e29540f82c240a
Todd Hayton <thayton@neekanee.com>
2014-06-10 10:14:43 -0400

    acquired

--- neekanee/job_scrapers/plugins/com/hrmdirect/nearinfinity.py
+++ neekanee/job_scrapers/plugins/com/hrmdirect/nearinfinity.py
@@ -23,50 +23,9 @@ class NearInfinityJobScraper(JobScraper):
     def __init__(self):
         super(NearInfinityJobScraper, self).__init__(COMPANY)
 
-    def scrape_job_links(self, url):
-        jobs = []
-
-        self.br.open(url)
-
-        s = soupify(self.br.response().read())
-        t = s.find('table', attrs={'class': 'reqResultTable'})
-        r = re.compile(r'^job-opening\.php\?req=\d+&#job')
-
-        for a in t.findAll('a', href=r):
-            job = Job(company=self.company)
-            job.title = a.text
-            job.url = urlparse.urljoin(self.br.geturl(), a['href'])
-            jobs.append(job)
-
-        return jobs
-
     def scrape_jobs(self):
-        job_list = self.scrape_job_links(self.company.jobs_page_url)
-        self.prune_unlisted_jobs(job_list)
-        new_jobs = self.new_job_listings(job_list)
-
-        for job in new_jobs:
-            self.br.open(job.url)
-
-            s = soupify(self.br.response().read())
-            x = {'class': 'reqResult'}
-            d = s.find('div', attrs=x)
-            t = d.find('td', text='Location:')
-            
-            if not t:
-                continue
-
-            tr = t.findParent('tr')
-            td = tr.findAll('td')
-            
-            l = self.parse_location(td[-1].text)
-
-            if not l:
-                continue
-
-            job.desc = get_all_text(d)
-            job.location = l
-            job.save()
+        # Now Altimera
+        self.company.job_set.all().delete()
 
 def get_scraper():
     return NearInfinityJobScraper()
