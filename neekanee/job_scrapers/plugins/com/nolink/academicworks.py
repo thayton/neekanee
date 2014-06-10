9dc1b8445b4493cc32955f27588388526afe8562
Todd Hayton <thayton@neekanee.com>
2014-06-10 10:12:16 -0400

    moved

--- neekanee/job_scrapers/plugins/com/nolink/academicworks.py
+++ neekanee/job_scrapers/plugins/com/nolink/academicworks.py
@@ -9,10 +9,8 @@ COMPANY = {
     'name': 'AcademicWorks',
     'hq': 'Austin, TX',
 
-    'benefits': {'vacation': []},
-
     'home_page_url': 'http://www.academicworks.com',
-    'jobs_page_url': 'http://www.academicworks.com/careers',
+    'jobs_page_url': 'http://www.academicworks.com/company/careers/',
 
     'empcnt': [1,10]
 }
@@ -25,23 +23,33 @@ class AcademicWorksJobScraper(JobScraper):
         self.br.open(self.company.jobs_page_url)
 
         s = soupify(self.br.response().read())
-        d = s.find('div', id='content')
-        t = d.find(text='Open Positions')
-        d = t.findParent('div')
-        a = {'class': 'toggle'}
+
+        x = {'class': 'tab-title-wrapper'}
+        d = s.find('div', attrs=x)
+
+        x = {'class': re.compile(r'tab-content-wrapper')}
+        w = s.find('div', attrs=x)
+
+        x = {'class': re.compile(r'tab-title')}
+        y = {'class': re.compile(r'tab-content')}
 
         self.company.job_set.all().delete()
 
-        for h6 in d.findAll('h6', attrs=a):
-            x = {'class': 'toggle_div'}
-            v = h6.findNext('div', attrs=x)
+        for h4 in d.findAll('h4', attrs=x):
+            i = d.index(h4)
+            tab_content_list = w.findAll('div', attrs=y)
+            tab_content = tab_content_list[i]
 
             job = Job(company=self.company)
-            job.title = h6.text
+            job.title = h4.text
             job.url = self.br.geturl()
             job.location = self.company.location
-            job.desc = get_all_text(v)
+            job.desc = get_all_text(tab_content)
             job.save()
 
 def get_scraper():
     return AcademicWorksJobScraper()
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
