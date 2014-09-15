77e75653cf8a359c3107fb3cf257537d80212efa
Todd Hayton <thayton@neekanee.com>
2014-09-15 15:20:49 -0400

    acquired by oracle

--- neekanee/job_scrapers/plugins/com/silkroad/acmepacket.py
+++ neekanee/job_scrapers/plugins/com/silkroad/acmepacket.py
@@ -22,46 +22,9 @@ class AcmePacketJobScraper(JobScraper):
     def __init__(self):
         super(AcmePacketJobScraper, self).__init__(COMPANY)
 
-    def scrape_job_links(self, url):
-        jobs = []
-
-        self.br.open(url)
-        self.br.select_form('frmsearch')
-        self.br.submit()
-
-        s = soupify(self.br.response().read())
-        r = re.compile(r'index\.cfm\?fuseaction=app.jobinfo&jobid=\d+')
-
-        for a in s.findAll('a', href=r):
-            tr = a.findParent('tr')
-            td = tr.findAll('td')
-
-            l = self.parse_location(td[2].text)
-            if not l:
-                continue
-
-            job = Job(company=self.company)
-            job.title = a.text
-            job.url = urlparse.urljoin(self.br.geturl(), a['href'])
-            job.url = url_query_filter(job.url, ['fuseaction', 'jobid', 'company_id'])
-            job.location = l
-            jobs.append(job)
-
-        return jobs
-
     def scrape_jobs(self):
-        job_list = self.scrape_job_links(self.company.jobs_page_url)
-        self.prune_unlisted_jobs(job_list)
-        new_jobs = self.new_job_listings(job_list)
-
-        for job in new_jobs:
-            self.br.open(job.url)
-
-            s = soupify(self.br.response().read())
-            f = s.find('form', id='applyJob')
-
-            job.desc = get_all_text(f)
-            job.save()
+        # Acquired by Oracle
+        self.company.job_set.all().delete()
 
 def get_scraper():
     return AcmePacketJobScraper()
