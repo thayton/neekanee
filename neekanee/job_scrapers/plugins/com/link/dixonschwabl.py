ce6411d025b64be830fd6ce1e001ad2178e763f9
Todd Hayton <thayton@neekanee.com>
2014-09-15 16:16:54 -0400

    dixonschwabl

---
+++ neekanee/job_scrapers/plugins/com/link/dixonschwabl.py
@@ -0,0 +1,61 @@
+import re, urlparse
+
+from neekanee.jobscrapers.jobscraper import JobScraper
+from neekanee.htmlparse.soupify import soupify, get_all_text
+
+from neekanee_solr.models import *
+
+COMPANY = {
+    'name': 'Dixon Schwabl',
+    'hq': 'Victor, NY',
+
+    'home_page_url': 'http://www.dixonschwabl.com',
+    'jobs_page_url': 'http://dixonschwabl.com/careers',
+
+    'empcnt': [51,200]
+}
+
+class DixonSchwablJobScraper(JobScraper):
+    def __init__(self):
+        super(DixonSchwablJobScraper, self).__init__(COMPANY)
+
+    def scrape_job_links(self, url):
+        jobs = []
+
+        self.br.open(url)
+
+        s = soupify(self.br.response().read())
+        r = re.compile(r'^/careers/[^/]+$')
+        x = {'class': 'opening', 'href': r}
+
+        for a in s.findAll('a', attrs=x):
+            job = Job(company=self.company)
+            job.title = a.text
+            job.location = self.company.location
+            job.url = urlparse.urljoin(self.br.geturl(), a['href'])
+            jobs.append(job)
+
+        return jobs
+
+    def scrape_jobs(self):
+        job_list = self.scrape_job_links(self.company.jobs_page_url)
+        self.prune_unlisted_jobs(job_list)
+        new_jobs = self.new_job_listings(job_list)
+
+        for job in new_jobs:
+            self.br.open(job.url)
+
+            s = soupify(self.br.response().read())
+            r = re.compile(r'careers_detail')
+            x = {'class': r}
+            d = s.find('div', attrs=x)
+
+            job.desc = get_all_text(d)
+            job.save()
+
+def get_scraper():
+    return DixonSchwablJobScraper()
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
