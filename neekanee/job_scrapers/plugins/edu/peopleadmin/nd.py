568b25b0ef51dc8803d3d9dd23edde2de4fef844
Todd Hayton <thayton@neekanee.com>
2014-09-19 11:32:12 -0400

    newer version of peopleadmin

--- neekanee/job_scrapers/plugins/edu/peopleadmin/nd.py
+++ neekanee/job_scrapers/plugins/edu/peopleadmin/nd.py
@@ -1,23 +1,18 @@
-from neekanee.jobscrapers.peopleadmin.PeopleAdminATS import PeopleAdminJobScraper
+from neekanee.jobscrapers.peopleadmin.peopleadmin3 import PeopleAdminJobScraper
 
 COMPANY = {
     'name': 'University of Notre Dame',
     'hq': 'Notre Dame, IN',
 
-    'benefits': {
-        'url': 'http://hr.nd.edu/benefits/',
-        'vacation': [(1,10),(3,15),(11,20),(21,25)],
-        'holidays': 12,
-        'tuition_assistance': True
-    },
-
     'home_page_url': 'http://www.nd.edu',
-    'jobs_page_url': 'https://jobs.nd.edu/applicants/jsp/shared/Welcome_css.jsp',
-
-    'gctw_chronicle': True,
+    'jobs_page_url': 'https://jobs.nd.edu',
 
     'empcnt': [1001,5000]
 }
 
 def get_scraper():
     return PeopleAdminJobScraper(COMPANY)
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
