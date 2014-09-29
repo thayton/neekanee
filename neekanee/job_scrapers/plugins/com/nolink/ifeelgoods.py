1c9883475bad1b38e27cac9b83602fb902f2202f
Todd Hayton <thayton@neekanee.com>
2014-09-29 11:24:12 -0400

    new layout

--- neekanee/job_scrapers/plugins/com/nolink/ifeelgoods.py
+++ neekanee/job_scrapers/plugins/com/nolink/ifeelgoods.py
@@ -10,7 +10,7 @@ COMPANY = {
     'hq': 'Menlo Park, CA',
 
     'home_page_url': 'http://www.ifeelgoods.com',
-    'jobs_page_url': 'http://www.ifeelgoods.com/about-us/careers/',
+    'jobs_page_url': 'http://www.ifeelgoods.com/careers/',
 
     'empcnt': [1,10]
 }
@@ -18,30 +18,29 @@ COMPANY = {
 class IFeelGoodsJobScraper(JobScraper):
     def __init__(self):
         super(IFeelGoodsJobScraper, self).__init__(COMPANY)
+        self.br.addheaders = [('User-agent', 
+                               'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7')]
 
     def scrape_jobs(self):
         self.br.open(self.company.jobs_page_url)
 
         s = soupify(self.br.response().read())
-        d = s.find('div', id='content-part')
-        x = {'class': 'toggle'}
+        x = {'class': 'spb_toggle'}
 
         self.company.job_set.all().delete()
 
-        for v in d.findAll('div', attrs=x):
-            z = v.findParent('div')
-            t = v.find('div', attrs={'class': 'title'})
+        for d in s.findAll('div', attrs=x):
+            l = d.text.split('-')[1]
+            l = self.parse_location(l)
 
-            job = Job(company=self.company)
-            job.title = t.text
-            job.url = urlparse.urljoin(self.br.geturl(), '#' + z['id'])
-
-            l = self.parse_location(v.h4.text)
             if not l:
                 continue
 
+            job = Job(company=self.company)
+            job.title = d.text
+            job.url = self.br.geturl()
             job.location = l
-            job.desc = get_all_text(z)
+            job.desc = get_all_text(d.parent)
             job.save()
 
 def get_scraper():
