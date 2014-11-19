a1ec906bb555eeacba8720c45dc0273e23744b92
Todd Hayton <thayton@neekanee.com>
2014-11-19 07:30:54 -0500

    exelisinc

--- neekanee/job_scrapers/plugins/com/brassring/exelisinc.py
+++ neekanee/job_scrapers/plugins/com/brassring/exelisinc.py
@@ -1,23 +1,35 @@
 import re
-from neekanee.jobscrapers.kenexa.kenexa import KenexaJobScraper
+
+from neekanee.jobscrapers.brassring.brassring import BrassringJobScraper
+from neekanee.htmlparse.soupify import soupify
 
 COMPANY = {
     'name': 'Exelis',
     'hq': 'McLean, VA',
 
     'home_page_url': 'http://www.exelisinc.com',
-    'jobs_page_url': 'https://sjobs.brassring.com/1033/ASP/TG/cim_home.asp?partnerid=25326&siteid=5443',
+    'jobs_page_url': 'https://sjobs.brassring.com/TGWebHost/home.aspx?partnerid=25326&siteid=5443',
 
     'empcnt': [10001]
 }
 
-class ExelisIncJobScraper(KenexaJobScraper):
+class ExelisIncJobScraper(BrassringJobScraper):
     def __init__(self):
         super(ExelisIncJobScraper, self).__init__(COMPANY)
         self.soupify_search_form = True
 
-    def get_location_from_td(self, td):
-        return self.parse_location(td[3].text)
+    def get_url_from_formtext(self, x):
+        s = soupify(x['AutoReq'])
+        return s.a
+
+    def get_title_from_formtext(self, x):
+        return x['FORMTEXT12']
+
+    def get_location_from_formtext(self, x):
+        l = x['FORMTEXT10']
+        l = self.parse_location(l)
+
+        return l
 
 def get_scraper():
     return ExelisIncJobScraper()
