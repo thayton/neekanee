c93f3dcab967cc1a6a0839649bcca51548704141
Todd Hayton <thayton@neekanee.com>
2014-11-24 10:39:58 -0500

    northeastrehab

--- neekanee/job_scrapers/plugins/com/hiredesk/northeastrehab.py
+++ neekanee/job_scrapers/plugins/com/hiredesk/northeastrehab.py
@@ -19,48 +19,9 @@ class NortheastRehabJobScraper(JobScraper):
     def __init__(self):
         super(NortheastRehabJobScraper, self).__init__(COMPANY, return_usa_only=False)
 
-    def scrape_job_links(self, url):
-        jobs = []
-
-        self.br.open(url)
-
-        s = soupify(self.br.response().read())
-        d = s.find('div', id='TPMainContentDiv')
-        r = re.compile(r'^/viewjobs/JobDetail\.asp\?')
-
-        for a in d.findAll('a', href=r):
-            tr = a.findParent('tr')
-            td = tr.findAll('td')
-
-            l = td[-3].text + ', ' + td[-2].text
-            l = self.parse_location(l)
-
-            if not l:
-                continue
-
-            job = Job(company=self.company)
-            job.title = a.text
-            job.url = urlparse.urljoin(self.br.geturl(), a['href'])
-            job.location = l
-            jobs.append(job)
-
-        return jobs
-
     def scrape_jobs(self):
-        job_list = self.scrape_job_links(self.company.jobs_page_url)
-        self.prune_unlisted_jobs(job_list)
-        new_jobs = self.new_job_listings(job_list)
-
-        for job in new_jobs:
-            self.br.open(job.url)
-
-            s = soupify(self.br.response().read())
-            d = s.find('div', id='TPMainContentDiv')
-            x = {'class': 'FormContent'}
-            t = d.find('table', attrs=x)
-
-            job.desc = get_all_text(t)
-            job.save()
+        # Site now forbids scraping
+        self.company.job_set.all().delete()
 
 def get_scraper():
     return NortheastRehabJobScraper()
