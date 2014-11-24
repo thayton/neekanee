ddbda1f2aba44529bf484aedcf68ab21da3c110f
Todd Hayton <thayton@neekanee.com>
2014-11-24 10:50:48 -0500

    winvale

--- neekanee/job_scrapers/plugins/com/nolink/winvale.py
+++ neekanee/job_scrapers/plugins/com/nolink/winvale.py
@@ -23,17 +23,29 @@ class WinValeJobScraper(JobScraper):
         self.br.open(self.company.jobs_page_url)
 
         s = soupify(self.br.response().read())
-        d = s.find('div', id='careers_sections')
+        x = {'class': 'tabs-nav'}
+        u = s.find('ul', attrs=x)
+
+        x = {'class': 'tabs-container'}
+        d = s.find('div', attrs=x)
+
+        r = re.compile(r'^#tab-[\d-]+$')
 
         self.company.job_set.all().delete()
 
-        for x in d.findAll('div', recursive=False):
+        for a in u.findAll('a', href=r):
+            v = d.find('div', id=a['href'][1:])
+            
             job = Job(company=self.company)
-            job.title = x.h2.text
+            job.title = a.text
             job.url = self.br.geturl()
             job.location = self.company.location
-            job.desc = get_all_text(x.div) 
+            job.desc = get_all_text(v) 
             job.save()
 
 def get_scraper():
     return WinValeJobScraper()
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
