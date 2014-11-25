5590e70a88e18402fc79d0d98ea393030bbd7fb7
Todd Hayton <thayton@neekanee.com>
2014-11-25 16:00:36 -0500

    yale

--- neekanee/job_scrapers/plugins/edu/kenexa/yale.py
+++ neekanee/job_scrapers/plugins/edu/kenexa/yale.py
@@ -1,35 +1,38 @@
-from neekanee.jobscrapers.kenexa.kenexa import KenexaJobScraper
+from neekanee.jobscrapers.brassring.brassring import BrassringJobScraper
+from neekanee.htmlparse.soupify import soupify
 
 COMPANY = {
     'name': 'Yale University',
     'hq': 'New Haven, CT',
 
-    'ats': 'Kenexa',
-
-    'benefits': {
-        'url': 'http://www.yale.edu/hronline/benefits/index.html',
-        'vacation': [(1,10),(5,15),(10,20),(20,25)],
-        'holidays': 16,
-        'sick_days': 12,
-        'tuition_assistance': True
-    },
-
     'home_page_url': 'http://www.yale.edu',
-    'jobs_page_url': 'https://sjobs.brassring.com/1033/ASP/TG/cim_home.asp?partnerid=25053&siteid=5248',
+    'jobs_page_url': 'https://sjobs.brassring.com/TGWebHost/home.aspx?partnerid=25053&siteid=5248',
 
     'empcnt': [10001]
 }
 
-class YaleJobScraper(KenexaJobScraper):
+class YaleJobScraper(BrassringJobScraper):
     def __init__(self):
         super(YaleJobScraper, self).__init__(COMPANY)
+        self.soupify_search_form = True
 
-    def get_title_from_td(self, td):
-        return td[5].text
+    def get_url_from_formtext(self, x):
+        s = soupify(x['JobTitle'])
+        return s.a
 
-    def update_frmAgent_form(self):
-        self.br.form.set_all_readonly(False)
-        self.br.form['Question27935__FORMDATE1'] = 'AnswerName&=|=X|%%%AnswerValue&=|=X|%%%GDEAnswerValue&=|=X|???'
+    def get_title_from_formtext(self, x):
+        s = soupify(x['JobTitle'])
+        return s.a.text
+
+    def get_location_from_formtext(self, x):
+        l = x['Location']
+        l = self.parse_location(l)
+
+        return l
 
 def get_scraper():
     return YaleJobScraper()
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
