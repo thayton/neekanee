80c0a40c4df392da14112659fb90800e39f9c546
Todd Hayton <thayton@neekanee.com>
2014-11-25 16:53:27 -0500

    mantech

--- neekanee/job_scrapers/plugins/com/brassring/mantech.py
+++ neekanee/job_scrapers/plugins/com/brassring/mantech.py
@@ -1,25 +1,36 @@
-from neekanee.jobscrapers.kenexa.kenexa import KenexaJobScraper
 import re
+
+from neekanee.jobscrapers.brassring.brassring import BrassringJobScraper
+from neekanee.htmlparse.soupify import soupify
 
 COMPANY = {
     'name': 'ManTech',
     'hq': 'Fairfax, VA',
 
     'home_page_url': 'http://www.mantech.com',
-    'jobs_page_url': 'http://jobs.brassring.com/1033/ASP/TG/cim_home.asp?partnerid=10696&siteid=45',
+    'jobs_page_url': 'http://jobs.brassring.com/TGWebHost/home.aspx?partnerid=10696&siteid=45',
 
     'empcnt': [5001,10000]
 }
 
-class ManTechJobScraper(KenexaJobScraper):
+class ManTechJobScraper(BrassringJobScraper):
     def __init__(self):
         super(ManTechJobScraper, self).__init__(COMPANY)
+        self.soupify_search_form = True
 
-    def get_location_from_td(self, td):
-        if len(td) == 0:
-            return None
-        else:
-            return self.parse_location(td[-4].text)
+    def get_url_from_formtext(self, x):
+        s = soupify(x['JobTitle'])
+        return s.a
+
+    def get_title_from_formtext(self, x):
+        s = soupify(x['JobTitle'])
+        a = s.findAll('a')
+        return a[1].text
+
+    def get_location_from_formtext(self, x):
+        l = x['Location']
+        l = self.parse_location(l)
+        return l
 
 def get_scraper():
     return ManTechJobScraper()
