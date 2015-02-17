df45afe14f98e51acd1af6ce300ba39c299a4969
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-02-17 09:50:58 -0500

    yammer

--- neekanee/job_scrapers/plugins/com/jobvite/yammer.py
+++ neekanee/job_scrapers/plugins/com/jobvite/yammer.py
@@ -22,48 +22,8 @@ class YammerJobScraper(JobScraper):
         super(YammerJobScraper, self).__init__(COMPANY)
 
     def scrape_job_links(self, url):
-        jobs = []
-
-        self.br.open(url)
-
-        s = soupify(self.br.response().read())
-        r = re.compile(r'/job_description\?jvi=\w+,Job')
-        v = { 'class': 'jvcontent' }
-        t = s.find('table', attrs=v)
-
-        for a in t.findAll('a', href=r):
-            l = self.parse_location(a.findNext('td').text)
-            if l is None:
-                continue
-
-            job = Job(company=self.company)
-            job.title = a.text
-            job.url = a['href']
-            job.location = l
-            jobs.append(job)
-
-        return jobs
-
-    def scrape_jobs(self):
-        job_list = self.scrape_job_links(self.company.jobs_page_url)
-        self.prune_unlisted_jobs(job_list)
-        new_jobs = self.new_job_listings(job_list)
-
-        for job in new_jobs:
-            self.br.open(job.url)
-
-            s = soupify(self.br.response().read())
-            x = job.url.find('jvi=')+4
-            j = '&j=' + job.url[x:]
-            l = s.iframe['src'] + j + '&k=Job'
-
-            self.br.open(l)
-
-            s = soupify(self.br.response().read())
-            d = s.find('div', attrs={'class': 'jvcontent'})
-
-            job.desc = get_all_text(d)
-            job.save()
+        # Acquired by Microsoft
+        self.company.job_set.all().delete()
 
 def get_scraper():
     return YammerJobScraper()
