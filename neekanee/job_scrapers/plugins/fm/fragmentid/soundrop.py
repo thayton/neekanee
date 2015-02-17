73973ad4309c77ff188babcdf1ff41c0c3e6177b
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-02-17 09:19:43 -0500

    soundrop

--- neekanee/job_scrapers/plugins/fm/fragmentid/soundrop.py
+++ neekanee/job_scrapers/plugins/fm/fragmentid/soundrop.py
@@ -19,25 +19,8 @@ class SoundropJobScraper(JobScraper):
         super(SoundropJobScraper, self).__init__(COMPANY)
 
     def scrape_jobs(self):
-        self.br.open(self.company.jobs_page_url)
-
-        s = soupify(self.br.response().read())
-        r = re.compile(r'^#\S+$')
-        f = lambda x: x.name == 'a' and x.has_key('href') and re.search(r, x['href']) and x.parent.name == 'li'
-
+        # OOB
         self.company.job_set.all().delete()
-
-        for a in s.findAll(f):
-            job = Job(company=self.company)
-            job.title = a.text
-            job.url = urlparse.urljoin(self.br.geturl(), a['href'])
-            job.location = self.company.location
-
-            h = s.find('h2', id=a['href'][1:])
-            v = h.findParent('article')
-
-            job.desc = get_all_text(v)
-            job.save()
 
 def get_scraper():
     return SoundropJobScraper()
