bba75fc3d0d051cf2656e730c6bbd5dc7280621a
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-02-19 10:51:19 -0500

    carwoo

--- neekanee/job_scrapers/plugins/com/resumator/carwoo.py
+++ neekanee/job_scrapers/plugins/com/resumator/carwoo.py
@@ -1,4 +1,9 @@
-from neekanee.jobscrapers.resumator.resumator import ResumatorJobScraper
+import re, urlparse
+
+from neekanee.jobscrapers.jobscraper import JobScraper
+from neekanee.htmlparse.soupify import soupify, get_all_text
+
+from neekanee_solr.models import *
 
 COMPANY = {
     'name': 'CarWoo',
@@ -10,5 +15,18 @@ COMPANY = {
     'empcnt': [5001,10000]
 }
 
+class CarWooJobScraper(JobScraper):
+    def __init__(self):
+        super(CarWooJobScraper, self).__init__(COMPANY)
+
+    def scrape_jobs(self):
+        # Acquired by TrueCar
+        self.company.job_set.all().delete()
+
 def get_scraper():
-    return ResumatorJobScraper(COMPANY)
+    return CarWooJobScraper()
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
+
