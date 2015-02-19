3eab7f9c25df1f019d99b6df90edf9474f8553ba
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-02-19 11:17:24 -0500

    itdp

--- neekanee/job_scrapers/plugins/org/pdf/itdp.py
+++ neekanee/job_scrapers/plugins/org/pdf/itdp.py
@@ -23,7 +23,7 @@ class ItdpJobScraper(JobScraper):
                                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7')]
 
     def scrape_jobs(self):
-        # HTML is too crappy
+        # HTML is too unstructured
         self.company.job_set.all().delete()
 
 def get_scraper():
