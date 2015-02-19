dcf65184b370992b88f2dc3ad113598fe818b858
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-02-19 11:17:10 -0500

    itdp

--- neekanee/job_scrapers/plugins/org/pdf/itdp.py
+++ neekanee/job_scrapers/plugins/org/pdf/itdp.py
@@ -22,47 +22,9 @@ class ItdpJobScraper(JobScraper):
         self.br.addheaders = [('User-agent', 
                                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7')]
 
-    def scrape_job_links(self, url):
-        jobs = []
-
-        self.br.open(url)
-
-        s = soupify(self.br.response().read())
-        r = re.compile(r'/uploads/[^\.]+\.pdf$')
-        x = {'font-weight': 'bold;'}
-
-        for a in s.findAll('a', href=r):
-            p = a.findPrevious('span')
-            h = a.findPrevious('h3')
-
-            m = re.search(re.compile(r'\(([^)]+)'), p.text)
-            if m:
-                l = m.group(1) + ', ' + h.text
-                l = self.parse_location(l)
-                if not l:
-                    continue
-
-            job = Job(company=self.company)
-            job.title = p.text
-            job.url = urlparse.urljoin(self.br.geturl(), a['href'])
-            job.location = l
-            jobs.append(job)
-
-        return jobs
-
     def scrape_jobs(self):
-        job_list = self.scrape_job_links(self.company.jobs_page_url)
-        self.prune_unlisted_jobs(job_list)
-        new_jobs = self.new_job_listings(job_list)
-
-        for job in new_jobs:
-            self.br.open(job.url)
-
-            d = self.br.response().read()
-            s = soupify(pdftohtml(d))
-
-            job.desc = get_all_text(s.html.body)
-            job.save()
+        # HTML is too crappy
+        self.company.job_set.all().delete()
 
 def get_scraper():
     return ItdpJobScraper()
