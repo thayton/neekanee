0e1d2a78bce4c5e51405359d7bf2d7503b8a3199
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-03-04 11:24:42 -0500

    fixed

--- neekanee/job_scrapers/plugins/edu/peopleadmin/uri.py
+++ neekanee/job_scrapers/plugins/edu/peopleadmin/uri.py
@@ -1,14 +1,18 @@
-from neekanee.jobscrapers.peopleadmin.PeopleAdminATS import PeopleAdminJobScraper
+from neekanee.jobscrapers.peopleadmin.peopleadmin3 import PeopleAdminJobScraper
 
 COMPANY = {
     'name': 'University of Rhode Island',
     'hq': 'Kingston, RI',
 
     'home_page_url': 'http://www.uri.edu',
-    'jobs_page_url': 'https://jobs.uri.edu/applicants/jsp/shared/Welcome_css.jsp',
+    'jobs_page_url': 'https://jobs.uri.edu',
 
     'empcnt': [1001,5000]
 }
 
 def get_scraper():
     return PeopleAdminJobScraper(COMPANY)
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
