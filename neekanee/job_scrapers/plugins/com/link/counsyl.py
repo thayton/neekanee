2822c3db59149c262d6867d389aaab4a2bede413
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-03-05 15:19:47 -0500

    fixed

--- neekanee/job_scrapers/plugins/com/link/counsyl.py
+++ neekanee/job_scrapers/plugins/com/link/counsyl.py
@@ -18,6 +18,8 @@ COMPANY = {
 class CounsylJobScraper(JobScraper):
     def __init__(self):
         super(CounsylJobScraper, self).__init__(COMPANY)
+        self.br.addheaders = [('User-agent', 
+                               'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7')]
 
     def scrape_job_links(self, url):
         jobs = []
@@ -25,16 +27,15 @@ class CounsylJobScraper(JobScraper):
         self.br.open(url)
 
         s = soupify(self.br.response().read())
-        x = {'class': 'joblist'}
-        r = re.compile(r'^/jobs/[^/]+/$')
+        d = s.find('div', id='positions')
+        r = re.compile(r'^/careers/[^/]+/$')
         
-        for u in s.findAll('ul', attrs=x):
-            for a in u.findAll('a', href=r):
-                job = Job(company=self.company)
-                job.title = a.text
-                job.url = urlparse.urljoin(self.br.geturl(), a['href'])
-                job.location = self.company.location
-                jobs.append(job)
+        for a in d.findAll('a', href=r):
+            job = Job(company=self.company)
+            job.title = a.text
+            job.url = urlparse.urljoin(self.br.geturl(), a['href'])
+            job.location = self.company.location
+            jobs.append(job)
 
         return jobs
 
@@ -47,14 +48,11 @@ class CounsylJobScraper(JobScraper):
             self.br.open(job.url)
 
             s = soupify(self.br.response().read())
-            d = s.find('div', id='content')
-            r = re.compile(r'^Location:')
-            t = d.find(text=r)
-            if t:
-                l = re.sub(r, '', t)
-                l = self.parse_location(l)
-                if l:
-                    job.location = l
+            d = s.h1.findParent('div')
+            l = self.parse_location(d.p.text)
+
+            if l:
+                job.location = l
 
             job.desc = get_all_text(d)
             job.save()
