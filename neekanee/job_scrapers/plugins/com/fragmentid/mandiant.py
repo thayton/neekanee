ca1569b4b4e7653d4f4fd92cfb78a7f3f849193d
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-03-06 16:37:50 -0500

    fixed

--- neekanee/job_scrapers/plugins/com/fragmentid/mandiant.py
+++ neekanee/job_scrapers/plugins/com/fragmentid/mandiant.py
@@ -21,35 +21,8 @@ class MandiantJobScraper(JobScraper):
                                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7')]
 
     def scrape_jobs(self):
-        self.br.open(self.company.jobs_page_url)
-
-        s = soupify(self.br.response().read())
-        r = re.compile(r'^job-')
-        z = {'class': 'job-loc'}
-        x = {'id': r, 'data-target': re.compile(r'^job-\d+$')} 
-        y = s.find('aside', id='job-locations')
-
+        # Acquired
         self.company.job_set.all().delete()
-
-        for a in y.findAll('a', attrs=x):
-            t = a['data-target']
-            d = s.find('div', id=t)
-            p = d.find('p', attrs=z)
-            l = self.parse_location(p.text)
-
-            if not l:
-                continue
-
-            b = os.path.dirname(a['href'])
-            p = os.path.basename(b)
-            b = os.path.dirname(b)
-
-            job = Job(company=self.company)
-            job.title = a.text
-            job.url = urlparse.urljoin(self.br.geturl(), b + '/#' + p)
-            job.location = l
-            job.desc = get_all_text(d)
-            job.save()
 
 def get_scraper():
     return MandiantJobScraper()
