4d56ca78c7ea5fd46aeb41bcd5beaf80d0590cc6
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-03-06 16:32:00 -0500

    fixed

--- neekanee/job_scrapers/plugins/com/resumator/wingu.py
+++ neekanee/job_scrapers/plugins/com/resumator/wingu.py
@@ -1,4 +1,9 @@
-from neekanee.jobscrapers.resumator.resumator import ResumatorJobScraper
+import re, urlparse
+
+from neekanee.jobscrapers.jobscraper import JobScraper
+from neekanee.htmlparse.soupify import soupify, get_all_text
+
+from neekanee_solr.models import *
 
 COMPANY = {
     'name': 'Wingu',
@@ -10,5 +15,17 @@ COMPANY = {
     'empcnt': [11,50]
 }
 
+class WinguJobScraper(JobScraper):
+    def __init__(self):
+        super(WinguJobScraper, self).__init__(COMPANY)
+
+    def scrape_jobs(self):
+        # Acquired
+        self.company.job_set.all().delete()
+
 def get_scraper():
-    return ResumatorJobScraper(COMPANY)
+    return WinguJobScraper()
+
+if __name__ == '__main__':
+    job_scraper = get_scraper()
+    job_scraper.scrape_jobs()
