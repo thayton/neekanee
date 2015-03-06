6f68078b9d2d7d1735967a0a16282d97dc60e9d3
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-03-06 16:37:09 -0500

    fixed

--- neekanee/job_scrapers/plugins/com/resumator/jumptap.py
+++ neekanee/job_scrapers/plugins/com/resumator/jumptap.py
@@ -1,4 +1,9 @@
-from neekanee.jobscrapers.resumator.resumator import ResumatorJobScraper
+import re, urlparse
+
+from neekanee.jobscrapers.jobscraper import JobScraper
+from neekanee.htmlparse.soupify import soupify, get_all_text
+
+from neekanee_solr.models import *
 
 COMPANY = {
     'name': 'Jumptap',
@@ -10,8 +15,16 @@ COMPANY = {
     'empcnt': [51,200]
 }
 
+class JumptapJobScraper(JobScraper):
+    def __init__(self):
+        super(JumptapJobScraper, self).__init__(COMPANY)
+
+    def scrape_jobs(self):
+        # Acquired
+        self.company.job_set.all().delete()
+
 def get_scraper():
-    return ResumatorJobScraper(COMPANY)
+    return JumptapJobScraper()
 
 if __name__ == '__main__':
     job_scraper = get_scraper()
